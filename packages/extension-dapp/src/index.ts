17c0501628bb85e9e590ec6a3d2fdc6863b35250
Jaco Greeff <jacogr@gmail.com>
2019-05-24 10:57:54 +0200

    Flatter promise

--- packages/extension-dapp/src/index.ts
+++ packages/extension-dapp/src/index.ts
@@ -24,10 +24,9 @@ export function web3Enable (originName: string): Promise<Array<InjectedExtension
         .catch(() => [{ name, version }, null] as [InjectedExtensionInfo, null])
     ))
     .then((values) =>
-      values.filter(([, r]) => r !== null) as Array<[InjectedExtensionInfo, Injected]>
-    )
-    .then((values) =>
-      values.map(([info, ext]) => ({ ...info, ...ext } as InjectedExtension))
+      values
+        .filter(([, r]) => r !== null)
+        .map(([info, ext]) => ({ ...info, ...ext } as InjectedExtension))
     )
     .catch(() => [] as Array<InjectedExtension>)
     .then((values) => {

d1b57b96fab8166eeaf4680345f7faeef508c129
Jaco Greeff <jacogr@gmail.com>
2019-07-12 20:58:49 +0200

    Update index.ts

--- packages/extension-inject/src/index.ts
+++ packages/extension-inject/src/index.ts
@@ -4,6 +4,9 @@
 
 import { Injected, InjectedWindow, InjectOptions } from './types';
 
+// It is recomemender to always use the function below to shield the extension and dapp from
+// any future changes. The exposed interface will manage access between the 2 environments,
+// be it via window (current), postMessage (under consideration) or any other mechanism
 export function injectExtension (enable: (origin: string) => Promise<Injected>, { name, version }: InjectOptions): void {
   // small helper with the typescript types, just cast window
   const windowInject = window as InjectedWindow;

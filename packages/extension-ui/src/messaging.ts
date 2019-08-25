d0de8c0f34c11d1506b68ff592c2ddc451919caa
Jaco Greeff <jacogr@gmail.com>
2019-08-25 11:44:23 +0200

    Add derivation path support (import) (#125)
    * Add derivation path support (import)
    
    * Fix params (we really need these properly typed!)
    
    * Bump to stable API
    
    * 0.8

--- packages/extension-ui/src/messaging.ts
+++ packages/extension-ui/src/messaging.ts
@@ -112,6 +112,6 @@ export async function subscribeSigning (cb: (accounts: SigningRequest[]) => void
   return sendMessage('signing.subscribe', {}, cb);
 }
 
-export async function validateSeed (seed: string, type?: KeypairType): Promise<{ address: string; seed: string }> {
-  return sendMessage('seed.validate', { seed, type });
+export async function validateSeed (suri: string, type?: KeypairType): Promise<{ address: string; suri: string }> {
+  return sendMessage('seed.validate', { suri, type });
 }

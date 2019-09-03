9540ed9c86da455714d380c03b5480599f1584b0
Jaco Greeff <jacogr@gmail.com>
2019-09-03 17:55:38 +0200

    Typo in cheain descriptions

--- packages/extension-chains/src/dev/polkadot-051.ts
+++ packages/extension-chains/src/dev/polkadot-051.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// Generates via (in api) `yarn chain:info ws://127.0.0.1:9944`
+// Generated via (in api) `yarn chain:info ws://127.0.0.1:9944`
 
 export default {
   chain: 'Polkadot Dev (v0.5.1)',

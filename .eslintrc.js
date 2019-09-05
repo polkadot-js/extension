0634b24090ce00dee3d99558b4e55672b44c396e
Jaco Greeff <jacogr@gmail.com>
2019-09-05 12:38:59 +0200

    Bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ const base = require('@polkadot/dev-react/config/eslint');
 module.exports = {
   ...base,
   parserOptions: {
-    ...base.parserOptions,
     extraFileExtensions: ['*.d.ts'],
     project: [
       './tsconfig.eslint.json'

85b9d2013a265828738c8f86b8f8dfc4a23b1c6e
Jaco Greeff <jacogr@gmail.com>
2019-09-05 12:54:17 +0200

    Align eslint config with other projects

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ const base = require('@polkadot/dev-react/config/eslint');
 module.exports = {
   ...base,
   parserOptions: {
-    extraFileExtensions: ['*.d.ts'],
+    ...base.parserOptions,
     project: [
       './tsconfig.eslint.json'
     ]

218bac09fe10c41e2c858dfc40101615205ac020
Jaco Greeff <jacogr@gmail.com>
2019-09-06 09:20:53 +0200

    Align with master naming

--- packages/extension/src/background/types.ts
+++ packages/extension/src/background/types.ts
@@ -7,11 +7,12 @@ import { SignerPayloadJSON } from '@polkadot/types/types';
 import { KeypairType } from '@polkadot/util-crypto/types';
 
 type KeysWithDefinedValues<T> = {
-  [P in keyof T]: undefined extends T[P] ? P : never
+  [K in keyof T]: undefined extends T[K] ? K : never
 }[keyof T];
 
-type IsNull<T, K extends keyof T> = { [K1 in Exclude<keyof T, K>]: T[K1] } & T[K] extends null ? K : never
-type NullKeys<T> = { [K in keyof T]: IsNull<T, K> }[keyof T]
+type IsNull<T, K extends keyof T> = { [K1 in Exclude<keyof T, K>]: T[K1] } & T[K] extends null ? K : never;
+
+type NullKeys<T> = { [K in keyof T]: IsNull<T, K> }[keyof T];
 
 export type SeedLengths = 12 | 24;
 

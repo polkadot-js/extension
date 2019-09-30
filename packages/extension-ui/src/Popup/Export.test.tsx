26c4e2358ddc90b04c426025a5adcced3f07ca42
Bartłomiej Rutkowski <bartek@ethworks.io>
2019-09-30 10:49:21 +0200

    test refactoring

--- packages/extension-ui/src/Popup/Export.test.tsx
+++ packages/extension-ui/src/Popup/Export.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router';
 import Adapter from 'enzyme-adapter-react-16';
-import { configure, mount } from 'enzyme';
+import { configure, mount, ReactWrapper } from 'enzyme';
 import { exportAccount } from '../messaging';
 import Export from './Export';
 import { act } from 'react-dom/test-utils';
@@ -12,30 +12,31 @@ jest.mock('../messaging');
 const flushAllPromises = (): Promise<void> => new Promise(resolve => setImmediate(resolve));
 
 describe('Export component', () => {
-  test('creates export message on button press', async () => {
+  let wrapper: ReactWrapper;
+
+  beforeEach(() => {
     (exportAccount as jest.Mock).mockResolvedValue({ exportedJson: 'json repr' });
-    const wrapper = mount(<MemoryRouter
+
+    wrapper = mount(<MemoryRouter
       initialEntries={ ['/account/export/HjoBp62cvsWDA3vtNMWxz6c9q13ReEHi9UGHK7JbZweH5g5'] }>
       <Route path='/account/export/:address' component={ Export }/>
     </MemoryRouter>);
+  });
 
+  it('creates export message on button press', async () => {
     wrapper.find('[data-export-password] input').simulate('change', { target: { value: 'passw0rd' } });
     wrapper.find('[data-export-button] button').simulate('click');
     await act(flushAllPromises);
+
     expect(exportAccount).toHaveBeenCalledWith('HjoBp62cvsWDA3vtNMWxz6c9q13ReEHi9UGHK7JbZweH5g5', 'passw0rd');
   });
 
-  test('shows text area with export json', async () => {
-    (exportAccount as jest.Mock).mockResolvedValue({ exportedJson: 'json repr' });
-    const wrapper = mount(<MemoryRouter
-      initialEntries={ ['/account/export/HjoBp62cvsWDA3vtNMWxz6c9q13ReEHi9UGHK7JbZweH5g5'] }>
-      <Route path='/account/export/:address' component={ Export }/>
-    </MemoryRouter>);
-
+  it('shows text area with export json', async () => {
     wrapper.find('[data-export-password] input').simulate('change', { target: { value: 'passw0rd' } });
     wrapper.find('[data-export-button] button').simulate('click');
     await act(flushAllPromises);
     wrapper.update();
+
     expect(wrapper.find('[data-exported-account] textarea').text()).toContain('json repr');
   });
 });

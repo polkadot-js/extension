60ce94b4cfe0f66be9c75339bed9ec27ba3a3c04
Bartłomiej Rutkowski <bartek@ethworks.io>
2019-10-04 15:50:38 +0200

    tests refactoring

--- packages/extension-ui/src/Popup/Accounts/Account.test.tsx
+++ packages/extension-ui/src/Popup/Accounts/Account.test.tsx
@@ -2,7 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AccountJson } from '@polkadot/extension/background/types';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure, mount, ReactWrapper } from 'enzyme';
 import { Link } from '@polkadot/extension-ui/components';
@@ -16,27 +15,28 @@ configure({ adapter: new Adapter() });
 describe('Account component', () => {
   let wrapper: ReactWrapper;
   const VALID_ADDRESS = 'HjoBp62cvsWDA3vtNMWxz6c9q13ReEHi9UGHK7JbZweH5g5';
-  const mountAccountComponent = (account: AccountJson): ReactWrapper => mount(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const mountAccountComponent = (additionalAccountProperties: any): ReactWrapper => mount(
     <MemoryRouter>
       <Account
-        {...account}>
+        {...{ address: VALID_ADDRESS, ...additionalAccountProperties }}>
       </Account>
     </MemoryRouter>);
 
   it('shows Export option if account is not external', () => {
-    wrapper = mountAccountComponent({ address: VALID_ADDRESS, isExternal: false });
+    wrapper = mountAccountComponent({ isExternal: false });
 
     expect(wrapper.find(Link).length).toBe(3);
-    expect(wrapper.find(Link).at(0).contains('Edit')).toBe(true);
-    expect(wrapper.find(Link).at(1).contains('Export')).toBe(true);
-    expect(wrapper.find(Link).at(2).contains('Forget')).toBe(true);
+    expect(wrapper.find(Link).at(0).text()).toContain('Edit');
+    expect(wrapper.find(Link).at(1).text()).toContain('Export');
+    expect(wrapper.find(Link).at(2).text()).toContain('Forget');
   });
 
   it('does not show Export option if account is external', () => {
-    wrapper = mountAccountComponent({ address: VALID_ADDRESS, isExternal: true });
+    wrapper = mountAccountComponent({ isExternal: true });
 
     expect(wrapper.find(Link).length).toBe(2);
-    expect(wrapper.find(Link).at(0).contains('Edit')).toBe(true);
-    expect(wrapper.find(Link).at(1).contains('Forget')).toBe(true);
+    expect(wrapper.find(Link).at(0).text()).toContain('Edit');
+    expect(wrapper.find(Link).at(1).text()).toContain('Forget');
   });
 });

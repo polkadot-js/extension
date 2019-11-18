de45181f200bbe157d5067b2911fa1336c583af3
Kamil Dąbrowski <kamil@ethworks.io>
2019-11-18 10:41:20 +0100

    change welcome screen (#19)

--- packages/extension-ui/src/Popup/Welcome.tsx
+++ packages/extension-ui/src/Popup/Welcome.tsx
@@ -7,19 +7,6 @@ import React, { useContext } from 'react';
 import { ActionContext, Box, Button, ButtonArea, Header, List, VerticalSpace } from '../components';
 import styled from 'styled-components';
 
-const WelcomeLabel = styled.div.attrs(() => ({
-  children: 'Welcome'
-}))`
-  margin: 30px auto 40px auto;
-  font-size: 24px;
-  line-height: 33px;
-`;
-
-const Note = styled.p`
-  margin-bottom: 6px;
-  margin-top: 0;
-`;
-
 type Props = {};
 
 export default function Welcome (): React.ReactElement<Props> {
@@ -35,13 +22,13 @@ export default function Welcome (): React.ReactElement<Props> {
       <Header/>
       <WelcomeLabel/>
       <Note>Before we start, just a couple of notes regarding use:</Note>
-      <Box>
+      <TextBox>
         <List>
           <li>We do not send any clicks, pageviews or events to a central server</li>
           <li>We do not use any trackers or analytics</li>
           <li>We don&apos;t collect keys, addresses or any information - your information never leaves this machine</li>
         </List>
-      </Box>
+      </TextBox>
       <Note>... we are not in the information collection business (even anonymized).</Note>
       <VerticalSpace/>
       <ButtonArea>
@@ -53,3 +40,21 @@ export default function Welcome (): React.ReactElement<Props> {
     </>
   );
 }
+
+const WelcomeLabel = styled.div.attrs(() => ({
+  children: 'Welcome'
+}))`
+  margin: 30px auto 40px auto;
+  font-size: 24px;
+  line-height: 33px;
+`;
+
+const Note = styled.p`
+  margin-bottom: 6px;
+  margin-top: 0;
+`;
+
+const TextBox = styled(Box)`
+  margin: 0.75rem 1rem;
+  border: 1px solid ${({ theme }): string => theme.inputBorder};
+`;

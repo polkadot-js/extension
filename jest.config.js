cfe5c54fde5294fc09d74651c42d59d01a6514bf
Ivan Rukhavets <ivanruch@gmail.com>
2019-11-25 17:58:28 +0300

    Move fileMock to __mocks__ (#35)

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = Object.assign({}, config, {
   moduleNameMapper: {
     '@polkadot/extension-(chains|dapp|inject|ui)(.*)$': '<rootDir>/packages/extension-$1/src/$2',
     '@polkadot/extension(.*)$': '<rootDir>/packages/extension/src/$1',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'empty/object'
   },
   modulePathIgnorePatterns: [

7f710d357a5e9fcf86e8bde5b6d35aa9b09da89c
Jaco Greeff <jacogr@gmail.com>
2020-03-15 06:51:15 +0100

    Webpack pnp plugin

--- packages/extension/webpack.config.js
+++ packages/extension/webpack.config.js
@@ -7,6 +7,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const CopyPlugin = require('copy-webpack-plugin');
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
 const ManifestPlugin = require('webpack-extension-manifest-plugin');
 
 const pkgJson = require('./package.json');
@@ -19,96 +20,97 @@ const packages = [
   'extension-ui'
 ];
 
-function createWebpack ({ alias = {}, context }) {
-  const ENV = process.env.NODE_ENV || 'development';
-  const isProd = ENV === 'production';
+const ENV = process.env.NODE_ENV || 'development';
+const isProd = ENV === 'production';
 
-  return {
-    context,
-    devtool: false,
-    entry: {
-      background: './src/background/index.ts',
-      content: './src/content.ts',
-      extension: './src/extension.ts',
-      page: './src/page/index.ts'
-    },
-    mode: ENV,
-    output: {
-      chunkFilename: '[name].js',
-      filename: '[name].js',
-      globalObject: '(typeof self !== \'undefined\' ? self : this)',
-      path: path.join(context, 'build')
-    },
-    resolve: {
-      alias,
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|ts|tsx)$/,
-          exclude: /(node_modules)/,
-          use: [
-            require.resolve('thread-loader'),
-            {
-              loader: require.resolve('babel-loader'),
-              options: require('@polkadot/dev/config/babel')
-            }
-          ]
-        },
-        {
-          test: [/\.svg$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.woff2?$/],
-          use: [
-            {
-              loader: require.resolve('url-loader'),
-              options: {
-                limit: 10000,
-                name: 'static/[name].[ext]'
-              }
-            }
-          ]
-        }
-      ]
-    },
-    node: {
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      child_process: 'empty',
-      dgram: 'empty',
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty'
-    },
-    performance: {
-      hints: false
-    },
-    plugins: [
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(ENV),
-          PKG_NAME: JSON.stringify(pkgJson.name),
-          PKG_VERSION: JSON.stringify(pkgJson.version)
-        }
-      }),
-      new CopyPlugin([{ from: 'public' }]),
-      new ManifestPlugin({
-        config: {
-          base: manifest,
-          extend: {
-            version: pkgJson.version.split('-')[0] // remove possible -beta.xx
-          }
-        }
-      })
-    ].filter((entry) => entry),
-    watch: !isProd
-  };
-}
-
-module.exports = createWebpack({
+module.exports = {
   context: __dirname,
-  alias: packages.reduce((alias, pkg) => {
-    alias[`@polkadot/${pkg}`] = path.resolve(__dirname, `../${pkg}/src`);
+  devtool: false,
+  entry: {
+    background: './src/background/index.ts',
+    content: './src/content.ts',
+    extension: './src/extension.ts',
+    page: './src/page/index.ts'
+  },
+  mode: ENV,
+  output: {
+    chunkFilename: '[name].js',
+    filename: '[name].js',
+    globalObject: '(typeof self !== \'undefined\' ? self : this)',
+    path: path.join(__dirname, 'build')
+  },
+  resolve: {
+    alias: packages.reduce((alias, pkg) => {
+      alias[`@polkadot/${pkg}`] = path.resolve(__dirname, `../${pkg}/src`);
 
-    return alias;
-  }, {})
-});
+      return alias;
+    }, {}),
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    plugins: [
+      PnpWebpackPlugin,
+    ]
+  },
+  resolveLoader: {
+    plugins: [
+      PnpWebpackPlugin.moduleLoader(module),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use: [
+          require.resolve('thread-loader'),
+          {
+            loader: require.resolve('babel-loader'),
+            options: require('@polkadot/dev/config/babel')
+          }
+        ]
+      },
+      {
+        test: [/\.svg$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.woff2?$/],
+        use: [
+          {
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: 10000,
+              name: 'static/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  node: {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    child_process: 'empty',
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  performance: {
+    hints: false
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(ENV),
+        PKG_NAME: JSON.stringify(pkgJson.name),
+        PKG_VERSION: JSON.stringify(pkgJson.version)
+      }
+    }),
+    new CopyPlugin([{ from: 'public' }]),
+    new ManifestPlugin({
+      config: {
+        base: manifest,
+        extend: {
+          version: pkgJson.version.split('-')[0] // remove possible -beta.xx
+        }
+      }
+    })
+  ].filter((entry) => entry),
+  watch: !isProd
+}

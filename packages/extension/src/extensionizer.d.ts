733c928159d29094231e596363487f50870423b6
Jaco Greeff <jacogr@gmail.com>
2020-03-26 18:09:46 +0100

    header

--- packages/extension/src/extensionizer.d.ts
+++ packages/extension/src/extensionizer.d.ts
@@ -1,3 +1,7 @@
+// Copyright 2019-2020 @polkadot/extension authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 /// <reference types="chrome"/>
 
 declare module 'extensionizer' {

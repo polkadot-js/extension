6d4881290a300fabb5846047f8938ad7b4cd6c96
ivan <ivanruch@gmail.com>
2020-04-02 17:39:18 +0300

    CR requests

--- packages/extension-ui/src/utils/buildHierarchy.test.ts
+++ packages/extension-ui/src/utils/buildHierarchy.test.ts
@@ -1,3 +1,7 @@
+// Copyright 2019-2020 @polkadot/extension-ui authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 import { AccountJson, AccountWithChildren } from '@polkadot/extension-base/background/types';
 import { buildHierarchy } from './buildHierarchy';
 
--- packages/extension-ui/src/utils/buildHierarchy.ts
+++ packages/extension-ui/src/utils/buildHierarchy.ts
@@ -1,3 +1,7 @@
+// Copyright 2019-2020 @polkadot/extension-ui authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 import { AccountJson, AccountWithChildren } from '@polkadot/extension-base/background/types';
 
 const isRoot = (account: AccountJson): boolean => !account.parentAddress;
@@ -18,6 +22,7 @@ export const accountWithChildren = (allAccounts: AccountJson[]) => (account: Acc
   };
 };
 
-export const buildHierarchy = (accounts: AccountJson[]): AccountWithChildren[] => accounts
-  .filter(isRoot)
-  .map(accountWithChildren(accounts));
+export const buildHierarchy = (accounts: AccountJson[]): AccountWithChildren[] =>
+  accounts
+    .filter(isRoot)
+    .map(accountWithChildren(accounts));

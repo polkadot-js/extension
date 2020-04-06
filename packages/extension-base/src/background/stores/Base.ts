8b3b59a2c6e6e26c9bf206898826c54a7942865d
Jaco Greeff <jacogr@gmail.com>
2020-04-06 09:52:55 +0200

    Pull in native AccountsStore

--- packages/extension-base/src/background/stores/Base.ts
+++ packages/extension-base/src/background/stores/Base.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import chrome from '@polkadot/extension-base/chrome';
+import chrome from '../../chrome';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type StoreValue = Record<string, any>;
--- packages/extension-base/src/background/stores/index.ts
+++ packages/extension-base/src/background/stores/index.ts
@@ -2,4 +2,5 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+export { default as AccountsStore } from './Accounts';
 export { default as MetadataStore } from './Metadata';

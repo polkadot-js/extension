fd14d2b8f35419b7e2aab62b667b2749859ff8e3
Jaco Greeff <jacogr@gmail.com>
2020-04-07 10:46:54 +0200

    Header eslint rule

--- __mocks__/chrome.ts
+++ __mocks__/chrome.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 @polkadot/extension-ui authors & contributors
+// Copyright 2019-2020 @polkadot/extension authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 

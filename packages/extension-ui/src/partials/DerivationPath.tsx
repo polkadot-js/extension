5cd25e044def247d64be4b50ae26759eeff0b407
Ivan Rukhavets <ivanruch@gmail.com>
2020-04-17 17:12:03 +0300

    Hide suri input behind a spoiler (#84)

--- packages/extension-ui/src/partials/DerivationPath.tsx
+++ packages/extension-ui/src/partials/DerivationPath.tsx
@@ -3,19 +3,25 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
 import { validateDerivationPath } from '@polkadot/extension-ui/messaging';
-import { InputWithLabel, ValidatedInput } from '../components';
+import { InputWithLabel, Svg, ValidatedInput } from '../components';
 import { Result } from '../validators';
+import arrowIcon from '../assets/arrow-down.svg';
+import gearIcon from '../assets/gear.svg';
 
 interface Props {
+  className?: string;
   onChange: (derivedAccount: { address: string; suri: string } | null) => void;
   parentAddress: string;
   parentPassword: string;
   defaultPath: string;
 }
 
-function DerivationPath ({ defaultPath, onChange, parentAddress, parentPassword }: Props): React.ReactElement<Props> {
+function DerivationPath ({ className, defaultPath, onChange, parentAddress, parentPassword }: Props): React.ReactElement<Props> {
   const [path, setPath] = useState<string>(defaultPath);
+  const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
+
   const isPathValid = useCallback(async (newPath: string): Promise<Result<string>> => {
     try {
       await validateDerivationPath(parentAddress, newPath, parentPassword);
@@ -26,23 +32,99 @@ function DerivationPath ({ defaultPath, onChange, parentAddress, parentPassword 
     }
   }, [path, parentAddress, parentPassword]);
 
+  const _onExpand = useCallback(() => setShowAdvancedOptions(!showAdvancedOptions), [showAdvancedOptions]);
+
   const _onChange = useCallback(async (newPath: string | null) => {
     newPath !== null && setPath(newPath);
     onChange(newPath === null ? null : await validateDerivationPath(parentAddress, newPath, parentPassword));
   }, [parentAddress, onChange, parentPassword]);
 
   return (
-    <ValidatedInput
-      component={InputWithLabel}
-      data-input-suri
-      defaultValue={defaultPath}
-      label='Derivation path'
-      onValidatedChange={_onChange}
-      placeholder='//hard/soft'
-      validator={isPathValid}
-      value={path}
-    />
+    <div className={className}>
+      <OptionsLabel onClick={_onExpand}>
+        <LabelWithIcon>
+          <GearIcon/>
+          <label>Advanced derivation options</label>
+        </LabelWithIcon>
+        <ArrowIcon isExpanded={showAdvancedOptions}/>
+      </OptionsLabel>
+      <Expandable isExpanded={showAdvancedOptions}>
+        <ValidatedInput
+          component={InputWithLabel}
+          data-input-suri
+          defaultValue={defaultPath}
+          label='Derivation path'
+          onValidatedChange={_onChange}
+          placeholder='//hard/soft'
+          validator={isPathValid}
+          value={path}
+        />
+      </Expandable>
+    </div>
   );
 }
 
-export default DerivationPath;
+interface ExpandableComponentProps {
+  isExpanded: boolean;
+}
+
+const ArrowIcon = styled(Svg).attrs(() => ({
+  src: arrowIcon
+}))<ExpandableComponentProps>`
+  height: 6px;
+  width: 8px;
+  margin-right: 6px;
+  align-self: center;
+  background: ${({ theme }): string => theme.subTextColor};
+  transform: ${({ isExpanded }): string => `rotate(${isExpanded ? '0' : '-90'}deg)`};
+  transition: 0.1s;
+`;
+
+const LabelWithIcon = styled.div`
+  display: flex;
+  flex-direction: row;
+  user-select: none;
+`;
+
+const GearIcon = styled(Svg).attrs(() => ({
+  src: gearIcon
+}))`
+  height: 12px;
+  width: 12px;
+  margin-right: 6px;
+  align-self: center;
+  background: ${({ theme }): string => theme.subTextColor};
+`;
+
+export const OptionsLabel = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+  &:hover {
+    cursor: pointer;
+    color: ${({ theme }): string => `${theme.subTextColor}`};
+  }
+  label{
+    font-size: 10px;
+    margin-top: 1px;
+    line-height: 10px;
+    letter-spacing: 0.04em;
+    font-weight: 800;
+    opacity: 0.65;
+    text-transform: uppercase;
+    &:hover {
+      cursor: pointer;
+      color: ${({ theme }): string => `${theme.subTextColor}`};
+    }
+  }
+`;
+
+const Expandable = styled.div<ExpandableComponentProps>`
+  visibility: ${({ isExpanded }): string => isExpanded ? 'visible' : 'hidden'};
+`;
+
+export default styled(DerivationPath)`
+  border-top: ${({ theme }): string => `1px solid ${theme.inputBorderColor}`};
+  padding-top: 10px;
+`;

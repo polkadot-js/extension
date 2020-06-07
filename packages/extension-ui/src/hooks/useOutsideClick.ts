a7132a95aae56690eb918edb903d44812a3b6750
Jaco Greeff <jacogr@gmail.com>
2020-06-07 12:47:53 +0200

    Adjust account menu position (#367)

--- packages/extension-ui/src/hooks/useOutsideClick.ts
+++ packages/extension-ui/src/hooks/useOutsideClick.ts
@@ -2,14 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { useEffect, RefObject } from 'react';
+import { useCallback, useEffect, RefObject } from 'react';
 
 export default function useOutsideClick (ref: RefObject<HTMLDivElement>, callback: () => void): void {
-  const handleClick = (e: MouseEvent): void => {
+  const handleClick = useCallback((e: MouseEvent): void => {
     if (ref.current && !ref.current.contains(e.target as HTMLInputElement)) {
       callback();
     }
-  };
+  }, [callback, ref]);
 
   useEffect(() => {
     document.addEventListener('click', handleClick);

6a8ee383ec88f2439f71a7a78f54f223a2eeab6a
Bilal Alghazi <balghazi@gmail.com>
2020-08-27 02:17:08 -0400

    chore: `AddAccount` cleanup

--- packages/extension-ui/src/Popup/Accounts/AddAccount.tsx
+++ packages/extension-ui/src/Popup/Accounts/AddAccount.tsx
@@ -1,21 +1,10 @@
-// Copyright 2019-2020 @polkadot/extension-ui authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-
-import { ThemeProps } from '../../types';
-
 import React, { useCallback, useContext } from 'react';
-import styled from 'styled-components';
 
 import { ActionContext } from '../../components';
 import { Header, Icon, Box, Heading, Text, Button } from '../../ui';
 import { SvgPolyNew } from '@polkadot/extension-ui/assets/images/icons';
 
-interface Props extends ThemeProps {
-  className?: string;
-}
-
-function AddAccount ({ className }: Props): React.ReactElement<Props> {
+function AddAccount (): React.ReactElement<Props> {
   const onAction = useContext(ActionContext);
 
   const onCreateAccount = useCallback(
@@ -48,7 +37,7 @@ function AddAccount ({ className }: Props): React.ReactElement<Props> {
           <Box pt="m" width={220}>
           <Heading variant="h4" color="white">
             Welcome to the  Polymesh Wallet!
-          </Heading>
+          </Heading> 
           </Box>
           <Box mt="s" mb="4">
             <Text variant="b2" color="white">
@@ -70,29 +59,4 @@ function AddAccount ({ className }: Props): React.ReactElement<Props> {
   );
 }
 
-export default React.memo(styled(AddAccount)(({ theme }: Props) => `
-  color: ${theme.textColor};
-  height: 100%;
-
-  h3 {
-    color: ${theme.textColor};
-    margin-top: 0;
-    font-weight: normal;
-    font-size: 24px;
-    line-height: 33px;
-    text-align: center;
-  }
-
-  > .image {
-    display: flex;
-    justify-content: center;
-  }
-
-  > .no-accounts p {
-    text-align: center;
-    font-size: 16px;
-    line-height: 26px;
-    margin: 0 30px;
-    color: ${theme.subTextColor};
-  }
-`));
+export default React.memo(AddAccount);

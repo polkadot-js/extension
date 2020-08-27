4c524ff3b7f5357997c12c9330eb644d6fb5466b
Bilal Alghazi <balghazi@gmail.com>
2020-08-27 01:56:40 -0400

    feat: add account skinning

--- packages/extension-ui/src/Popup/Accounts/AddAccount.tsx
+++ packages/extension-ui/src/Popup/Accounts/AddAccount.tsx
@@ -8,8 +8,8 @@ import React, { useCallback, useContext } from 'react';
 import styled from 'styled-components';
 
 import { ActionContext } from '../../components';
-import Header from '../../partials/Header';
-import AddAccountImage from './AddAccountImage';
+import { Header, Icon, Box, Heading, Text, Button } from '../../ui';
+import { SvgPolyNew } from '@polkadot/extension-ui/assets/images/icons';
 
 interface Props extends ThemeProps {
   className?: string;
@@ -17,25 +17,55 @@ interface Props extends ThemeProps {
 
 function AddAccount ({ className }: Props): React.ReactElement<Props> {
   const onAction = useContext(ActionContext);
-  const _onClick = useCallback(
+
+  const onCreateAccount = useCallback(
     (): void => onAction('/account/create'),
+    [onAction]
+  );
+
+  const onImportSeed = useCallback(
+    (): void => onAction('/account/import-seed'),
+    [onAction]
+  );
+
+  const onImportJson = useCallback(
+    (): void => onAction('/account/restore-json'),
     [onAction]
   );
 
   return (
     <>
-      <Header
-        showSettings
-        text='Add Account'
-      />
-      <div className={className}>
-        <div className='image'>
-          <AddAccountImage onClick={_onClick}/>
-        </div>
-        <div className='no-accounts'>
-          <p>You currently don&apos;t have any accounts. Create your first account to get started.</p>
-        </div>
-      </div>
+      <Header>
+        <Box mt="l" pb="5">
+          <Box padding={13} borderRadius="50%" width={80} height={80} borderColor="white" borderWidth={4} border="solid" backgroundColor="brandLightest">
+            <Icon 
+              Asset={SvgPolyNew}
+              width={50}
+              height={50}
+              color="brandMain"
+            />
+          </Box>
+          <Box pt="m" width={220}>
+          <Heading variant="h4" color="white">
+            Welcome to the  Polymesh Wallet!
+          </Heading>
+          </Box>
+          <Box mt="s" mb="4">
+            <Text variant="b2" color="white">
+            Manage your Polymesh digital assets by creating an account or signing in to an existing account below. 
+            </Text>
+          </Box>
+        </Box>
+      </Header>
+      <Box mt="m">
+      <Button fluid onClick={onCreateAccount}>Create account</Button>
+      </Box>
+      <Box mt="s">
+      <Button fluid variant="secondary" onClick={onImportSeed}>Import account from seed</Button>
+      </Box>
+      <Box mt="s">
+      <Button fluid variant="ghost" onClick={onImportJson}>Import account from JSON</Button>
+      </Box>
     </>
   );
 }

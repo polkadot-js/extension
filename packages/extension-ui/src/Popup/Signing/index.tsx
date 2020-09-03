10e1eee36ca9e7f8c85ecbf365c57bf4c6928825
Jaco Greeff <jacogr@gmail.com>
2020-09-03 17:12:43 +0200

    Protect against requestIndex over/under (#423)

--- packages/extension-ui/src/Popup/Signing/index.tsx
+++ packages/extension-ui/src/Popup/Signing/index.tsx
@@ -2,26 +2,18 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { SigningRequest } from '@polkadot/extension-base/background/types';
+import { SignerPayloadJSON } from '@polkadot/types/types';
 
-import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 
 import { Loading, SigningReqContext } from '../../components';
 import { Header } from '../../partials';
 import Request from './Request';
 import TransactionIndex from './TransactionIndex';
 
-function isInnerTransaction (signingRequest: SigningRequest): boolean {
-  return 'blockNumber' in signingRequest.request.payload;
-}
-
 export default function Signing (): React.ReactElement {
   const requests = useContext(SigningReqContext);
   const [requestIndex, setRequestIndex] = useState(0);
-  const isTransaction = useMemo(
-    () => isInnerTransaction(requests[requestIndex]),
-    [requests, requestIndex]
-  );
 
   const _onNextClick = useCallback(
     () => setRequestIndex((requestIndex) => requestIndex + 1),
@@ -35,32 +27,44 @@ export default function Signing (): React.ReactElement {
 
   useEffect(() => {
     setRequestIndex(
-      (requestIndex) => requestIndex >= requests.length
-        ? requests.length - 1
-        : requestIndex
+      (requestIndex) => requestIndex < requests.length
+        ? requestIndex
+        : requests.length - 1
     );
   }, [requests]);
 
-  return requests[requestIndex] ? (
-    <>
-      <Header text={isTransaction ? 'Transaction' : 'Sign message'}>
-        {requests.length > 1 && (
-          <TransactionIndex
-            index={requestIndex}
-            onNextClick={_onNextClick}
-            onPreviousClick={_onPreviousClick}
-            totalItems={requests.length}
-          />
-        )}
-      </Header>
-      <Request
-        account={requests[requestIndex].account}
-        buttonText={isTransaction ? 'Sign the transaction' : 'Sign the message'}
-        isFirst={requestIndex === 0}
-        request={requests[requestIndex].request}
-        signId={requests[requestIndex].id}
-        url={requests[requestIndex].url}
-      />
-    </>
-  ) : <Loading />;
+  // protect against removal overflows/underflows
+  const request = requests.length !== 0
+    ? requestIndex >= 0
+      ? requestIndex < requests.length
+        ? requests[requestIndex]
+        : requests[requests.length - 1]
+      : requests[0]
+    : null;
+  const isTransaction = !!((request?.request?.payload as SignerPayloadJSON)?.blockNumber);
+
+  return request
+    ? (
+      <>
+        <Header text={isTransaction ? 'Transaction' : 'Sign message'}>
+          {requests.length > 1 && (
+            <TransactionIndex
+              index={requestIndex}
+              onNextClick={_onNextClick}
+              onPreviousClick={_onPreviousClick}
+              totalItems={requests.length}
+            />
+          )}
+        </Header>
+        <Request
+          account={request.account}
+          buttonText={isTransaction ? 'Sign the transaction' : 'Sign the message'}
+          isFirst={requestIndex === 0}
+          request={request.request}
+          signId={request.id}
+          url={request.url}
+        />
+      </>
+    )
+    : <Loading />;
 }

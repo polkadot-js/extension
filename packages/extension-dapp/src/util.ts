19782caa7c65c47fb5ad69f8da5eb8203037580c
Jaco Greeff <jacogr@gmail.com>
2020-10-18 10:12:21 +0200

    Only use document complete

--- packages/extension-dapp/src/util.ts
+++ packages/extension-dapp/src/util.ts
@@ -3,7 +3,7 @@
 
 export function documentReadyPromise <T> (creator: () => Promise<T>): Promise<T> {
   return new Promise((resolve): void => {
-    if (['complete', 'interactive'].includes(document.readyState)) {
+    if (document.readyState === 'complete') {
       resolve(creator());
     } else {
       window.addEventListener('load', (): void => {

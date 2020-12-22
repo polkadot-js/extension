5816eae7f9ce0a8078ba143741f25209ed6fd024
Thibaut Sardan <hi@thib.top>
2020-12-22 02:09:57 +0100

    extend with account not from context

--- packages/extension-ui/src/components/Address.test.tsx
+++ packages/extension-ui/src/components/Address.test.tsx
@@ -15,10 +15,12 @@ import { ThemeProvider } from 'styled-components';
 
 import { IconTheme } from '@polkadot/react-identicon/types';
 import defaultSettings from '@polkadot/ui-settings';
+import { KeypairType } from '@polkadot/util-crypto/types';
 
 import * as messaging from '../messaging';
 import { flushAllPromises } from '../testHelpers';
 import { buildHierarchy } from '../util/buildHierarchy';
+import { DEFAULT_TYPE } from '../util/defaultType';
 import { Address, SettingsContext } from '.';
 import { AccountContext, themes } from './';
 
@@ -28,6 +30,12 @@ configure({ adapter: new Adapter() });
 interface AccountTestJson extends AccountJson {
   expectedIconTheme: IconTheme
 }
+
+interface TestAccountidentifier {
+  name: string;
+  type: KeypairType;
+}
+
 const accounts = [
   { address: '5HSDXAC3qEMkSzZK377sTD1zJhjaPiX5tNWppHx2RQMYkjaJ', expectedIconTheme: 'polkadot', name: 'ECDSA Account', type: 'ecdsa' },
   { address: '5FjgD3Ns2UpnHJPVeRViMhCttuemaRXEqaD8V5z4vxcsUByA', expectedIconTheme: 'polkadot', name: 'Ed Account', type: 'ed25519' },
@@ -45,19 +53,19 @@ const mountComponent = async (props: Props, settings: Partial<SettingsStruct> = 
   const newSettings = { ...defaultSettings.get(), ...settings };
 
   const wrapper = mount(
-    <SettingsContext.Provider value={newSettings}>
-      <AccountContext.Provider value={{
-        accounts: accounts,
-        hierarchy: buildHierarchy(accounts)
-      }}>
-        <ThemeProvider theme={themes.dark}>
-          <Address
-            actions={actions}
-            {...props}
-          />
-        </ThemeProvider>
-      </AccountContext.Provider>
-    </ SettingsContext.Provider>
+    // <SettingsContext.Provider value={newSettings}>
+    <AccountContext.Provider value={{
+      accounts: accounts,
+      hierarchy: buildHierarchy(accounts)
+    }}>
+      <ThemeProvider theme={themes.dark}>
+        <Address
+          actions={actions}
+          {...props}
+        />
+      </ThemeProvider>
+    </AccountContext.Provider>
+    // </ SettingsContext.Provider>
   );
 
   await act(flushAllPromises);
@@ -67,18 +75,29 @@ const mountComponent = async (props: Props, settings: Partial<SettingsStruct> = 
 
 const genericTestSuite = (account: AccountTestJson) => {
   let wrapper: ReactWrapper;
-  const { address, expectedIconTheme, name, type } = account;
+  const { address, expectedIconTheme } = account;
+  const contextAccount = accounts.find((account) => account.address === address);
 
-  name && type && describe(`Displays an account from its address (${name}) - ${type}`, () => {
+  // if the account is in the context the expected attributes are retrieved from there.
+  // the Address component will only get to know about the address
+  // otherwise the account props will show more.
+  const expectedAttributes = {
+    name: contextAccount?.name || account.name || '<unknown>',
+    type: contextAccount?.type || account.type || DEFAULT_TYPE
+  };
+
+  describe(`Displays an account from its address (${expectedAttributes.name}) - ${expectedAttributes.type}`, () => {
     beforeEach(async () => {
-      const mountedComponent = await mountComponent(account);
+      const mountedComponent = contextAccount
+        ? await mountComponent({ address })
+        : await mountComponent(account);
 
       wrapper = mountedComponent.wrapper;
     });
 
     it('shows the account address and name', () => {
       expect(wrapper.find('[data-field="address"]').text()).toEqual(address);
-      expect(wrapper.find('Name span').text()).toEqual(name);
+      expect(wrapper.find('Name span').text()).toEqual(expectedAttributes.name);
     });
 
     it(`shows a ${expectedIconTheme} identicon`, () => {
@@ -109,7 +128,12 @@ const genericTestSuite = (account: AccountTestJson) => {
     });
 
     it('can show the account if hidden', async () => {
-      const mountedHiddenComponent = await mountComponent({ ...account, isHidden: true });
+      const additionalProps = { isHidden: true };
+
+      const mountedHiddenComponent = contextAccount
+        ? await mountComponent({ address, ...additionalProps })
+        : await mountComponent({ ...account, ...additionalProps });
+
       const wrapperHidden = mountedHiddenComponent.wrapper;
 
       jest.spyOn(messaging, 'showAccount').mockResolvedValue(true);
@@ -129,7 +153,11 @@ const genericTestSuite = (account: AccountTestJson) => {
     });
 
     it('has no account hidding and settings button if no action is provided', async () => {
-      const mountedComponentWithoutAction = await mountComponent({ ...account, actions: null });
+      const additionalProps = { actions: null };
+
+      const mountedComponentWithoutAction = contextAccount
+        ? await mountComponent({ address, ...additionalProps })
+        : await mountComponent({ ...account, ...additionalProps });
 
       wrapper = mountedComponentWithoutAction.wrapper;
 
@@ -137,6 +165,7 @@ const genericTestSuite = (account: AccountTestJson) => {
     });
   });
 };
+// };
 
 describe('Address', () => {
   // const uncheck = (input: ReactWrapper): unknown => input.simulate('change', { target: { checked: false } });
@@ -153,137 +182,21 @@ describe('Address', () => {
     genericTestSuite(account);
   });
 
-  //   it('"Create new root" button becomes visible after checkbox is unchecked', () => {
-  //     uncheck(wrapper.find('input[type="checkbox"]'));
+  const accountNotInContext = {
+    address: '5GYQRJj3NUznYDzCduENRcocMsyxmb6tjb5xW87ZMErBe9R7',
+    expectedIconTheme: 'polkadot',
+    name: 'Not in context',
+    type: 'sr25519'
+  } as AccountTestJson;
 
-  //     expect(wrapper.exists('[data-button-action="create root account"]')).toBe(true);
-  //     expect(wrapper.exists('[data-button-action="create derived account"]')).toBe(false);
-  //   });
+  genericTestSuite(accountNotInContext);
 
-  //   it('Password entry is hidden after checkbox is unchecked', () => {
-  //     uncheck(wrapper.find('input[type="checkbox"]'));
+  const EthereumAccountNotInContext = {
+    address: '0x4F4e24A11185565D483Ea1CFac1d3B96Fb7df684',
+    expectedIconTheme: 'ethereum',
+    name: 'Ethereum not in context',
+    type: 'ethereum'
+  } as AccountTestJson;
 
-  //     expect(wrapper.exists('input[type="password"]')).toBe(false);
-  //   });
-
-  //   it('"Create new root" button redirects to /account/create', () => {
-  //     uncheck(wrapper.find('input[type="checkbox"]'));
-  //     wrapper.find('[data-button-action="create root account"] button').simulate('click');
-
-  //     expect(onActionStub).toBeCalledWith('/account/create');
-  //   });
-
-  //   it('"Create derived account" is visible when checkbox is checked', () => {
-  //     expect(wrapper.exists('[data-button-action="create derived account"]')).toBe(true);
-  //     expect(wrapper.exists('[data-button-action="create root account"]')).toBe(false);
-  //   });
-
-  //   it('No error is visible when first loading the page', () => {
-  //     expect(wrapper.find('Warning')).toHaveLength(0);
-  //   });
-
-  //   it('"Create derived account" is disabled when password is not set', () => {
-  //     const button = wrapper.find('[data-button-action="create derived account"] button');
-
-  //     expect(button.prop('disabled')).toBe(true);
-  //   });
-
-  //   it('An error is visible and "Create derived account" is disabled when password is incorrect', async () => {
-  //     await type(wrapper.find('input[type="password"]'), 'wrong_pass');
-  //     wrapper.find('[data-button-action="create derived account"] button').simulate('click');
-  //     await act(flushAllPromises);
-  //     wrapper.update();
-
-  //     const button = wrapper.find('[data-button-action="create derived account"] button');
-
-  //     expect(button.prop('disabled')).toBe(true);
-  //     expect(wrapper.find('.warning-message')).toHaveLength(1);
-  //     expect(wrapper.find('.warning-message').first().text()).toEqual('Wrong password');
-  //   });
-
-  //   it('The error disappears when typing a new password and "Create derived account" is enabled', async () => {
-  //     await type(wrapper.find('input[type="password"]'), 'wrong_pass');
-  //     wrapper.find('[data-button-action="create derived account"] button').simulate('click');
-  //     await act(flushAllPromises);
-  //     wrapper.update();
-
-  //     await type(wrapper.find('input[type="password"]'), 'new_attempt');
-
-  //     const button = wrapper.find('[data-button-action="create derived account"] button');
-
-  //     expect(button.prop('disabled')).toBe(false);
-  //     expect(wrapper.find('.warning-message')).toHaveLength(0);
-  //   });
-
-  //   it('"Create derived account" is enabled when password is set', async () => {
-  //     await type(wrapper.find('input[type="password"]'), 'pass');
-
-  //     const button = wrapper.find('[data-button-action="create derived account"] button');
-
-  //     expect(button.prop('disabled')).toBe(false);
-  //     expect(wrapper.find('.warning-message')).toHaveLength(0);
-  //   });
-
-  //   it('An error is visible and "Create derived account" is disabled when suri is incorrect', async () => {
-  //     await type(wrapper.find('input[type="password"]'), 'pass');
-  //     await type(wrapper.find('[data-input-suri] input'), '//');
-  //     wrapper.find('[data-button-action="create derived account"] button').simulate('click');
-  //     await act(flushAllPromises);
-  //     wrapper.update();
-
-  //     const button = wrapper.find('[data-button-action="create derived account"] button');
-
-  //     expect(button.prop('disabled')).toBe(true);
-  //     expect(wrapper.find('.warning-message')).toHaveLength(1);
-  //     expect(wrapper.find('.warning-message').first().text()).toEqual('Incorrect derivation path');
-  //   });
-
-  //   it('The error disappears and "Create derived account" is enabled when typing a new suri', async () => {
-  //     await type(wrapper.find('input[type="password"]'), 'pass');
-  //     await type(wrapper.find('[data-input-suri] input'), '//');
-  //     wrapper.find('[data-button-action="create derived account"] button').simulate('click');
-  //     await act(flushAllPromises);
-  //     wrapper.update();
-  //     await type(wrapper.find('[data-input-suri] input'), 'new');
-
-  //     const button = wrapper.find('[data-button-action="create derived account"] button');
-
-  //     expect(button.prop('disabled')).toBe(false);
-  //     expect(wrapper.find('Warning')).toHaveLength(0);
-  //   });
-
-  //   it('takes selected address from URL as parent account', () => {
-  //     expect(wrapper.find('[data-field="name"]').first().text()).toBe('B');
-  //   });
-
-  //   it('selects internal root accounts as other options', () => {
-  //     const options = wrapper.find('[data-parent-option] [data-field="name"]').map((el) => el.text());
-
-  //     expect(options).toEqual(['A', 'B', 'D']);
-  //   });
-
-  //   it('redirects to derive from next account when other option is selected', () => {
-  //     wrapper.find('[data-parent-option]').first().simulate('click');
-
-  //     expect(onActionStub).toBeCalledWith(`/account/derive/${accounts[0].address}`);
-  //   });
+  genericTestSuite(EthereumAccountNotInContext);
 });
-
-// describe('Locked parent selection', () => {
-//   beforeAll(async () => {
-//     wrapper = (await mountComponent(true)).wrapper;
-//   });
-
-//   it('checkbox does not exist', () => {
-//     expect(wrapper.exists('[type="checkbox"]')).toBe(false);
-//   });
-
-//   it('address dropdown does not exist', () => {
-//     expect(wrapper.exists(AddressDropdown)).toBe(false);
-//   });
-
-//   it('parent is taken from URL', () => {
-//     expect(wrapper.find('[data-field="name"]').first().text()).toBe('B');
-//   });
-// });
-// });

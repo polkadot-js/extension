93405bf235bd83ee14ae0c91fb67aac0361f40ae
Jaco Greeff <jacogr@gmail.com>
2021-09-17 16:18:04 +0300

    Bump util

--- packages/extension-dapp/src/wrapBytes.ts
+++ packages/extension-dapp/src/wrapBytes.ts
@@ -1,9 +1,9 @@
 // Copyright 2019-2021 @polkadot/extension authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { U8A_WRAP_ETHEREUM, U8A_WRAP_POSTFIX, U8A_WRAP_PREFIX, u8aIsWrapped, u8aWrapBytes, u8aUnwrapBytes } from '@polkadot/util';
+import { U8A_WRAP_ETHEREUM, U8A_WRAP_POSTFIX, U8A_WRAP_PREFIX, u8aIsWrapped, u8aUnwrapBytes, u8aWrapBytes } from '@polkadot/util';
 
-export const ETHEREUM = U8A_WRAP_ETHEREUM
+export const ETHEREUM = U8A_WRAP_ETHEREUM;
 export const POSTFIX = U8A_WRAP_POSTFIX;
 export const PREFIX = U8A_WRAP_PREFIX;
 

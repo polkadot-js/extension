e4735b697e621f544ed8369785182a5d876b570b
estienne.antoine@gmail.com <estienne.antoine@gmail.com>
2021-12-07 14:50:47 +0100

    add eth test for extension signature

--- packages/extension-base/src/background/handlers/Extension.test.ts
+++ packages/extension-base/src/background/handlers/Extension.test.ts
@@ -8,6 +8,7 @@ import type { MetadataDef } from '@polkadot/extension-inject/types';
 import type { KeyringPair } from '@polkadot/keyring/types';
 import type { ExtDef } from '@polkadot/types/extrinsic/signedExtensions/types';
 import type { SignerPayloadJSON } from '@polkadot/types/types';
+import type { KeypairType } from '@polkadot/util-crypto/types';
 
 import { TypeRegistry } from '@polkadot/types';
 import keyring from '@polkadot/ui-keyring';
@@ -23,6 +24,7 @@ describe('Extension', () => {
   let state: State;
   let tabs: Tabs;
   const suri = 'seed sock milk update focus rotate barely fade car face mechanic mercy';
+  // const ETH_SURI = 'seed sock milk update focus rotate barely fade car face mechanic mercy' + '/m/44\'/60\'/0\'/0/0';
   const password = 'passw0rd';
 
   async function createExtension (): Promise<Extension> {
@@ -45,13 +47,21 @@ describe('Extension', () => {
     return new Extension(state);
   }
 
-  const createAccount = async (): Promise<string> => {
-    await extension.handle('id', 'pri(accounts.create.suri)', {
+  const createAccount = async (type?:KeypairType): Promise<string> => {
+    await extension.handle('id', 'pri(accounts.create.suri)', type&&type==="ethereum"?{
+      name: 'parent',
+      password,
+      suri,
+      type
+    }:{
       name: 'parent',
       password,
       suri
     }, {} as chrome.runtime.Port);
-    const { address } = await extension.handle('id', 'pri(seed.validate)', {
+    const { address } = await extension.handle('id', 'pri(seed.validate)',type&&type==="ethereum"?{
+      suri,
+      type
+    }: {
       suri
     }, {} as chrome.runtime.Port);
 
@@ -182,10 +192,10 @@ describe('Extension', () => {
         nonce: '0x0000000000000000',
         signedExtensions: ['CheckSpecVersion', 'CheckTxVersion', 'CheckGenesis', 'CheckMortality', 'CheckNonce', 'CheckWeight', 'ChargeTransactionPayment'],
         specVersion: '0x00000026',
-        tip: null,
+        tip: "0x00000000000000000000000000000000",
         transactionVersion: '0x00000005',
         version: 4
-      } as unknown as SignerPayloadJSON;
+      }
     });
 
     test('signs with default signed extensions', async () => {
@@ -197,6 +207,52 @@ describe('Extension', () => {
         .createType('ExtrinsicPayload', payload, { version: payload.version }).sign(pair);
 
       tabs.handle('1615191860871.5', 'pub(extrinsic.sign)', payload, 'http://localhost:3000', {} as chrome.runtime.Port)
+        .then((result) => {
+          expect((result as ResponseSigning)?.signature).toEqual(signatureExpected.signature);
+        }).catch((err) => console.log(err));
+
+      await expect(extension.handle('1615192072290.7', 'pri(signing.approve.password)', {
+        id: state.allSignRequests[0].id,
+        password,
+        savePass: false
+      }, {} as chrome.runtime.Port)).resolves.toEqual(true);
+    });
+
+    test('signs with default signed extensions - ethereum', async () => {
+      const eth_address = await createAccount("ethereum");
+      console.log("eth_address",eth_address)
+      const eth_pair = keyring.getPair(eth_address);
+      eth_pair.decodePkcs8(password);
+      const eth_payload:SignerPayloadJSON = {
+        specVersion: '0x000003e9',
+        transactionVersion:'0x00000002', //'0x00000002',
+        address: eth_address,
+        blockHash: '0xf9fc354edc3ff49f43d5e2c14e3c609a0c4ba469ed091edf893d672993dc9bc0',
+        blockNumber:'0x00000393', //'0x00000000',
+        era: '0x3601',//'0x0700',
+        genesisHash: '0xf9fc354edc3ff49f43d5e2c14e3c609a0c4ba469ed091edf893d672993dc9bc0',
+        method: '0x03003cd0a705a2dc65e5b1e1205896baa2be8a07c6e0070010a5d4e8',
+        nonce: '0x00000000',
+        signedExtensions: [
+          'CheckSpecVersion',
+          'CheckTxVersion',
+          'CheckGenesis',
+          'CheckMortality',
+          'CheckNonce',
+          'CheckWeight',
+          'ChargeTransactionPayment'
+        ],
+        tip: '0x00000000000000000000000000000000',
+        version: 4
+      }
+      const registry = new TypeRegistry();
+
+      registry.setSignedExtensions(payload.signedExtensions);
+
+      const signatureExpected = registry
+        .createType('ExtrinsicPayload', eth_payload, { version: eth_payload.version }).sign(eth_pair);
+
+      tabs.handle('1615191860871.5', 'pub(extrinsic.sign)', eth_payload, 'http://localhost:3000', {} as chrome.runtime.Port)
         .then((result) => {
           expect((result as ResponseSigning)?.signature).toEqual(signatureExpected.signature);
         }).catch((err) => console.log(err));
@@ -235,7 +291,7 @@ describe('Extension', () => {
 
       state.saveMetadata(meta);
 
-      const payload = {
+      const payload:SignerPayloadJSON = {
         address,
         blockHash: '0xe1b1dda72998846487e4d858909d4f9a6bbd6e338e4588e5d809de16b1317b80',
         blockNumber: '0x00000393',
@@ -245,10 +301,10 @@ describe('Extension', () => {
         nonce: '0x0000000000000000',
         signedExtensions: ['MyUserExtension'],
         specVersion: '0x00000026',
-        tip: null,
+        tip: "0x00000000000000000000000000000000",
         transactionVersion: '0x00000005',
         version: 4
-      } as unknown as SignerPayloadJSON;
+      } // as unknown as SignerPayloadJSON;
 
       const registry = new TypeRegistry();
 

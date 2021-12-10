34d19336335e5b86495fb6c50f4d9b00c95375b2
Jaco <jacogr@gmail.com>
2021-12-10 10:57:51 +0200

    Adjust

--- packages/extension-base/src/background/handlers/helpers.ts
+++ packages/extension-base/src/background/handlers/helpers.ts
@@ -1,12 +1,12 @@
 // Copyright 2019-2021 @polkadot/extension authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export function withErrorLog (fn: () => Promise<unknown>): void {
+export function withErrorLog (fn: () => unknown): void {
   try {
     const p = fn();
 
-    if (typeof p !== 'undefined' && typeof p.catch !== 'undefined') {
-      p.catch(console.error);
+    if (p && typeof p === 'object' && typeof (p as Promise<unknown>).catch === 'function') {
+      (p as Promise<unknown>).catch(console.error);
     }
   } catch (e) {
     console.error(e);

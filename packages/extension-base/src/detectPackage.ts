52c9666dff984cf33e75c824809c9ad78912388a
Jaco <jacogr@gmail.com>
2021-12-27 14:25:44 +0100

    Add detectPackage for extension-* (#946)
    * Bump deps
    
    * Add detectPackage for extension-*
    
    * Add missing util deps

---
+++ packages/extension-base/src/detectPackage.ts
@@ -0,0 +1,11 @@
+// Copyright 2017-2021 @polkadot/extension-base authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { packageInfo as chainsInfo } from '@polkadot/extension-chains/packageInfo';
+import { packageInfo as dappInfo } from '@polkadot/extension-dapp/packageInfo';
+import { packageInfo as injectInfo } from '@polkadot/extension-inject/packageInfo';
+import { detectPackage } from '@polkadot/util';
+
+import { packageInfo } from './packageInfo';
+
+detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [chainsInfo, dappInfo, injectInfo]);

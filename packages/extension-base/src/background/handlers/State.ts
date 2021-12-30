344091d48b090135ab39cf19b6181f6475adac92
Jaco <jacogr@gmail.com>
2021-12-30 11:59:20 +0100

    Adjust metadata loop with allMap (#953)
    * Adjust metadata loop with allMap
    
    * Remove debug log

--- packages/extension-base/src/background/handlers/State.ts
+++ packages/extension-base/src/background/handlers/State.ts
@@ -87,35 +87,39 @@ export enum NotificationOptions {
 const AUTH_URLS_KEY = 'authUrls';
 
 function extractMetadata (store: MetadataStore): void {
-  const knownEntries = Object.entries(knownGenesis);
-  const defs: Record<string, { def: MetadataDef, index: number, key: string }> = {};
-  const removals: string[] = [];
+  store.allMap((map): void => {
+    const knownEntries = Object.entries(knownGenesis);
+    const defs: Record<string, { def: MetadataDef, index: number, key: string }> = {};
+    const removals: string[] = [];
 
-  store.all((key: string, def: MetadataDef): void => {
-    const entry = knownEntries.find(([, hashes]) => hashes.includes(def.genesisHash));
+    Object
+      .entries(map)
+      .forEach(([key, def]): void => {
+        const entry = knownEntries.find(([, hashes]) => hashes.includes(def.genesisHash));
 
-    if (entry) {
-      const [name, hashes] = entry;
-      const index = hashes.indexOf(def.genesisHash);
+        if (entry) {
+          const [name, hashes] = entry;
+          const index = hashes.indexOf(def.genesisHash);
 
-      // flatten the known metadata based on the genesis index
-      // (lower is better/newer)
-      if (!defs[name] || (defs[name].index > index)) {
-        if (defs[name]) {
-          // remove the old version of the metadata
-          removals.push(defs[name].key);
+          // flatten the known metadata based on the genesis index
+          // (lower is better/newer)
+          if (!defs[name] || (defs[name].index > index)) {
+            if (defs[name]) {
+              // remove the old version of the metadata
+              removals.push(defs[name].key);
+            }
+
+            defs[name] = { def, index, key };
+          }
+        } else {
+          // this is not a known entry, so we will just apply it
+          defs[key] = { def, index: 0, key };
         }
+      });
 
-        defs[name] = { def, index, key };
-      }
-    } else {
-      // this is not a known entry, so we will just apply it
-      defs[key] = { def, index: 0, key };
-    }
+    removals.forEach((key) => store.remove(key));
+    Object.values(defs).forEach(({ def }) => addMetadata(def));
   });
-
-  removals.forEach((key) => store.remove(key));
-  Object.values(defs).forEach(({ def }) => addMetadata(def));
 }
 
 export default class State {
--- packages/extension-base/src/stores/Base.ts
+++ packages/extension-base/src/stores/Base.ts
@@ -19,15 +19,29 @@ export default abstract class BaseStore <T> {
   }
 
   public all (update: (key: string, value: T) => void): void {
+    this.allMap((map): void => {
+      Object.entries(map).forEach(([key, value]): void => {
+        update(key, value);
+      });
+    });
+  }
+
+  public allMap (update: (value: Record<string, T>) => void): void {
     chrome.storage.local.get(null, (result: StoreValue): void => {
       lastError('all');
 
-      Object
-        .entries(result)
-        .filter(([key]) => key.startsWith(this.#prefix))
-        .forEach(([key, value]): void => {
-          update(key.replace(this.#prefix, ''), value as T);
-        });
+      const entries = Object.entries(result);
+      const map: Record<string, T> = {};
+
+      for (let i = 0; i < entries.length; i++) {
+        const [key, value] = entries[i];
+
+        if (key.startsWith(this.#prefix)) {
+          map[key.replace(this.#prefix, '')] = value as T;
+        }
+      }
+
+      update(map);
     });
   }
 

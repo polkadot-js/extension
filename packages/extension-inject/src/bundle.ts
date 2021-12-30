e71c11e72b7283f0d01914e09f585586139b1e63
Jaco <jacogr@gmail.com>
2021-12-30 10:19:46 +0100

    Add bundle exports for packages with detect (#951)

---
+++ packages/extension-inject/src/bundle.ts
@@ -0,0 +1,24 @@
+// Copyright 2019-2021 @polkadot/extension-inject authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { Injected, InjectedWindow, InjectOptions } from './types';
+
+export { packageInfo } from './packageInfo';
+
+// It is recommended to always use the function below to shield the extension and dapp from
+// any future changes. The exposed interface will manage access between the 2 environments,
+// be it via window (current), postMessage (under consideration) or any other mechanism
+export function injectExtension (enable: (origin: string) => Promise<Injected>, { name, version }: InjectOptions): void {
+  // small helper with the typescript types, just cast window
+  const windowInject = window as Window & InjectedWindow;
+
+  // don't clobber the existing object, we will add it (or create as needed)
+  windowInject.injectedWeb3 = windowInject.injectedWeb3 || {};
+
+  // add our enable function
+  windowInject.injectedWeb3[name] = {
+    enable: (origin: string): Promise<Injected> =>
+      enable(origin),
+    version
+  };
+}
--- packages/extension-inject/src/index.ts
+++ packages/extension-inject/src/index.ts
@@ -3,24 +3,4 @@
 
 import './detectPackage';
 
-import type { Injected, InjectedWindow, InjectOptions } from './types';
-
-export { packageInfo } from './packageInfo';
-
-// It is recommended to always use the function below to shield the extension and dapp from
-// any future changes. The exposed interface will manage access between the 2 environments,
-// be it via window (current), postMessage (under consideration) or any other mechanism
-export function injectExtension (enable: (origin: string) => Promise<Injected>, { name, version }: InjectOptions): void {
-  // small helper with the typescript types, just cast window
-  const windowInject = window as Window & InjectedWindow;
-
-  // don't clobber the existing object, we will add it (or create as needed)
-  windowInject.injectedWeb3 = windowInject.injectedWeb3 || {};
-
-  // add our enable function
-  windowInject.injectedWeb3[name] = {
-    enable: (origin: string): Promise<Injected> =>
-      enable(origin),
-    version
-  };
-}
+export * from './bundle';

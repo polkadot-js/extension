0836c10e07f133fb1e5c5077f3ebce2fc5a07759
Jaco <jacogr@gmail.com>
2021-12-31 18:50:31 +0200

    2022

--- packages/extension-ui/src/Popup/Accounts/AddAccountImage.tsx
+++ packages/extension-ui/src/Popup/Accounts/AddAccountImage.tsx
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 @polkadot/extension-ui authors & contributors
+// Copyright 2019-2022 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 // We _could_ reformat, but just keep it as-is, since this is actually

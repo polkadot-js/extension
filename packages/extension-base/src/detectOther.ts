6dfebf68ade1d5c76f409d1571f483b3b5869745
Jaco <jacogr@gmail.com>
2021-12-31 17:17:08 +0200

    introduce detectOther

---
+++ packages/extension-base/src/detectOther.ts
@@ -0,0 +1,8 @@
+// Copyright 2017-2021 @polkadot/extension-base authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { packageInfo as chainsInfo } from '@polkadot/extension-chains/packageInfo';
+import { packageInfo as dappInfo } from '@polkadot/extension-dapp/packageInfo';
+import { packageInfo as injectInfo } from '@polkadot/extension-inject/packageInfo';
+
+export default [chainsInfo, dappInfo, injectInfo];
---
+++ packages/extension-dapp/src/detectOther.ts
@@ -0,0 +1,6 @@
+// Copyright 2017-2021 @polkadot/extension-dapp authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { packageInfo as injectInfo } from '@polkadot/extension-inject/packageInfo';
+
+export default [injectInfo];

607f4b3e3b045020659587771fd3eba7b3214862
Jaco <jacogr@gmail.com>
2022-01-01 08:26:35 +0100

    2022 (#957)

--- packages/extension-chains/src/cjs/dirname.d.ts
+++ packages/extension-chains/src/cjs/dirname.d.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2021 @polkadot/extension-chains authors & contributors
+// Copyright 2017-2022 @polkadot/extension-chains authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 declare const __dirname: string | undefined;

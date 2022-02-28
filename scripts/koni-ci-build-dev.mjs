f1458a2e8d6049d6d155371d726d3ca50c15784c
AnhMTV <maithachvietanh@gmail.com>
2022-02-28 12:37:33 +0700

    Update build workflow for koni-dev

---
+++ scripts/koni-ci-build-dev.mjs
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+// Copyright 2017-2022 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import fs from 'fs';
+import path from 'path';
+
+import execSync from '@polkadot/dev/scripts/execSync.mjs';
+import {Webhook} from "discord-webhook-node";
+
+console.log('$ polkadot-ci-ghact-build', process.argv.slice(2).join(' '));
+
+const discordHook = new Webhook(`https://discordapp.com/api/webhooks/${process.env.DISCORD_WEBHOOK_ID}/${process.env.DISCORD_WEBHOOK_TOKEN}`);
+
+function runClean () {
+  execSync('yarn polkadot-dev-clean-build');
+}
+
+function runCheck () {
+  execSync('yarn lint');
+}
+
+function runTest () {
+  execSync('yarn test');
+}
+
+function runBuild () {
+  execSync('yarn build');
+}
+
+function npmGetVersion () {
+  return JSON.parse(
+    fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8')
+  ).version;
+}
+
+function uploadBuild(){
+  const nowTimestamp = new Date().getTime()
+  const newName = `build-${npmGetVersion()}-${nowTimestamp}`
+  fs.renameSync('./master-build.zip', './' + newName)
+  discordHook.sendFile(file)
+}
+
+
+runClean();
+runCheck();
+runTest();
+runBuild();
+
+uploadBuild()

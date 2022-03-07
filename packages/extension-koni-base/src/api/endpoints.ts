f424e56dab0d97e255ebbb7fddcf48f37971c8d4
AnhMTV <maithachvietanh@gmail.com>
2022-03-07 21:34:24 +0700

    Rename 'Bit.Country - Testnet' => 'Bit.Country - Testnet'

--- packages/extension-koni-base/src/api/endpoints.ts
+++ packages/extension-koni-base/src/api/endpoints.ts
@@ -583,7 +583,7 @@ const NETWORKS: Record<string, NetWorkInfo> = {
     crowdloanUrl: 'https://polkadot.js.org/apps/#/parachains/crowdloan'
   },
   bitcountry: {
-    chain: 'Bit.Country - Metaverse Network',
+    chain: 'Bit.Country - Testnet',
     genesisHash: '0xfff6fd94251f570d4c9cdf25a0475da0d7ad35160290da19dad8f9caf8bf31b5',
     ss58Format: 42,
     provider: 'wss://tewai-rpc.bit.country',

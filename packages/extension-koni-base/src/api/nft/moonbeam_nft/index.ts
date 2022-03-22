f71f10a1a1821125d2b2aac029717848d1e5372a
Nam Phạm <namphamc99@gmail.com>
2022-03-22 17:16:22 +0700

    fix es-lint

--- packages/extension-koni-base/src/api/nft/moonbeam_nft/index.ts
+++ packages/extension-koni-base/src/api/nft/moonbeam_nft/index.ts
@@ -4,11 +4,11 @@
 import { ApiProps } from '@polkadot/extension-base/background/KoniTypes';
 import { BaseNftApi } from '@polkadot/extension-koni-base/api/nft/nft';
 
-const SUPPORTED_NFT_CONTRACTS = [
-  '0xCc1A7573C8f10d0df7Ee4d57cc958C8Df4a5Aca9', // Moon monkey
-  '0xe6e0696F70c507749d59283C0887E795Ee6Ff64b', // MoonDAO
-  '0x8fbe243d898e7c88a6724bb9eb13d746614d23d6' // GlimmerApes
-];
+// const SUPPORTED_NFT_CONTRACTS = [
+//   '0xCc1A7573C8f10d0df7Ee4d57cc958C8Df4a5Aca9', // Moon monkey
+//   '0xe6e0696F70c507749d59283C0887E795Ee6Ff64b', // MoonDAO
+//   '0x8fbe243d898e7c88a6724bb9eb13d746614d23d6' // GlimmerApes
+// ];
 
 export class MoonbeamNftApi extends BaseNftApi {
   // eslint-disable-next-line no-useless-constructor

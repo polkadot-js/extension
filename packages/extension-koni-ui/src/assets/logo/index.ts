c993e5e88fb95812011b6674bc89d013686bca40
Hieu Dao <daodinhhieu@gmail.com>
2022-04-16 23:42:16 +0700

    Issue #117: update chaosdao logo
    Issue #117: update chaosdao logo
    
    (cherry picked from commit b79f4b40bdc59249d380c204a109a67810772144)

--- packages/extension-koni-ui/src/assets/logo/index.ts
+++ packages/extension-koni-ui/src/assets/logo/index.ts
@@ -249,6 +249,8 @@ const LogosMap: Record<string, string> = {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   bill: require('./bill.svg'),
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  chaos: require('./chaosdao.jpeg'),
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   default: require('./default.svg')
 };
 

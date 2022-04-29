949ae3858c800083d2c911ba322216b212404003
AnhMTV <maithachvietanh@gmail.com>
2022-04-29 15:51:12 +0700

    Remove unused content #169

--- packages/extension/src/extension.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2019-2022 @polkadot/extension authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import '@polkadot/extension-inject/crossenv';
-
-import { createView, Popup } from '@polkadot/extension-ui';
-
-createView(Popup);

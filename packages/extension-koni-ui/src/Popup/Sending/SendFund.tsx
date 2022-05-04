5f644a6043ea16b5ae8e27bf646130acc0b64c55
quangdo <quangdm@cdmteck.com>
2022-05-04 11:04:35 +0700

    Refactor code SendFund, Donate

--- packages/extension-koni-ui/src/Popup/Sending/SendFund.tsx
+++ packages/extension-koni-ui/src/Popup/Sending/SendFund.tsx
@@ -5,8 +5,6 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-import { ChainRegistry, TokenInfo } from '@polkadot/extension-base/background/KoniTypes';
-import { AccountJson } from '@polkadot/extension-base/background/types';
 import { ethereumChains } from '@polkadot/extension-koni-base/api/dotsama/api-helper';
 import { AccountContext, Warning } from '@polkadot/extension-koni-ui/components';
 import Button from '@polkadot/extension-koni-ui/components/Button';
@@ -22,11 +20,11 @@ import { checkTransfer, transferCheckReferenceCount, transferCheckSupporting, tr
 import Header from '@polkadot/extension-koni-ui/partials/Header';
 import AuthTransaction from '@polkadot/extension-koni-ui/Popup/Sending/AuthTransaction';
 import SendFundResult from '@polkadot/extension-koni-ui/Popup/Sending/SendFundResult';
+import getDefaultValue, { getBalanceFormat, getMainTokenInfo, getMaxTransferAndNoFees } from '@polkadot/extension-koni-ui/Popup/Sending/utils';
 import { RootState } from '@polkadot/extension-koni-ui/stores';
 import { ThemeProps, TransferResultType } from '@polkadot/extension-koni-ui/types';
-import { isAccountAll } from '@polkadot/extension-koni-ui/util';
 import { checkAddress } from '@polkadot/phishing';
-import { BN, BN_HUNDRED, BN_ZERO } from '@polkadot/util';
+import { BN, BN_ZERO } from '@polkadot/util';
 import { isEthereumAddress } from '@polkadot/util-crypto';
 
 interface Props extends ThemeProps {
@@ -36,93 +34,6 @@ interface Props extends ThemeProps {
 interface ContentProps extends ThemeProps {
   className?: string;
   defaultValue: SenderInputAddressType;
-}
-
-function getDefaultAddress (address: string, accounts: AccountJson[]): string {
-  return isAccountAll(address) ? accounts[1].address : address;
-}
-
-function getDefaultToken (networkKey: string, chainRegistryMap: Record<string, ChainRegistry>): [string, string] | null {
-  const firstNetworkKey = Object.keys(chainRegistryMap)[0];
-
-  if (networkKey === 'all'
-    ? (!firstNetworkKey || !chainRegistryMap[firstNetworkKey])
-    : !chainRegistryMap[networkKey]) {
-    return null;
-  }
-
-  const token = networkKey === 'all' ? chainRegistryMap[firstNetworkKey].chainTokens[0] : chainRegistryMap[networkKey].chainTokens[0];
-
-  return networkKey === 'all' ? [firstNetworkKey, token] : [networkKey, token];
-}
-
-function getDefaultValue (
-  networkKey: string,
-  isCurrentNetworkInfoReady: boolean,
-  address: string | undefined,
-  chainRegistryMap: Record<string, ChainRegistry>,
-  accounts: AccountJson[]): SenderInputAddressType | null {
-  if (!address || !isCurrentNetworkInfoReady) {
-    return null;
-  }
-
-  const defaultToken = getDefaultToken(networkKey, chainRegistryMap);
-
-  if (!defaultToken) {
-    return null;
-  }
-
-  return {
-    address: getDefaultAddress(address, accounts),
-    networkKey: defaultToken[0],
-    token: defaultToken[1]
-  };
-}
-
-function getBalanceFormat (networkKey: string, token: string, chainRegistryMap: Record<string, ChainRegistry>): [number, string] {
-  const tokenInfo = chainRegistryMap[networkKey].tokenMap[token];
-
-  return [tokenInfo.decimals, tokenInfo.symbol];
-}
-
-function getPartialFee (fee: string | null, feeSymbol: string | null | undefined, selectedToken: string, mainTokenSymbol: string): BN {
-  if (!fee) {
-    return new BN('0');
-  }
-
-  if (feeSymbol) {
-    if (feeSymbol !== selectedToken) {
-      return new BN('0');
-    }
-  } else {
-    // feeSymbol is null or undefined => use mainTokenSymbol
-    if (selectedToken !== mainTokenSymbol) {
-      return new BN('0');
-    }
-  }
-
-  return new BN(fee);
-}
-
-function getMaxTransferAndNoFees (
-  fee: string | null,
-  feeSymbol: string | null | undefined,
-  selectedToken: string,
-  mainTokenSymbol: string,
-  senderFreeBalance: string,
-  existentialDeposit: string): [BN | null, boolean] {
-  const partialFee = getPartialFee(fee, feeSymbol, selectedToken, mainTokenSymbol);
-  const adjFee = partialFee.muln(110).div(BN_HUNDRED);
-  const maxTransfer = (new BN(senderFreeBalance)).sub(adjFee);
-
-  return maxTransfer.gt(new BN(existentialDeposit))
-    ? [maxTransfer, false]
-    : [null, true];
-}
-
-function getMainTokenInfo (networkKey: string, chainRegistryMap: Record<string, ChainRegistry>): TokenInfo {
-  // chainRegistryMap always has main token
-  return Object.values(chainRegistryMap[networkKey].tokenMap).find((t) => t.isMainToken) as TokenInfo;
 }
 
 function Wrapper ({ className = '', theme }: Props): React.ReactElement<Props> {

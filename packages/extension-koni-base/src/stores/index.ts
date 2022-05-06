f7d754e123d4080653dfb22a68fa15c293ce086f
Nam Phạm <namphamc99@gmail.com>
2022-05-06 11:46:30 +0700

    set evm contracts for nft API

--- packages/extension-koni-base/src/stores/index.ts
+++ packages/extension-koni-base/src/stores/index.ts
@@ -3,3 +3,4 @@
 
 export { default as PriceStore } from './Price';
 export { default as CurrentAccountStore } from './CurrentAccountStore';
+export { default as CustomEvmTokenStore } from './CustomEvmToken';

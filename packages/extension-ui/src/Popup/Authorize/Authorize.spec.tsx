e7996d6a884a94318baae1914d3eab9df60af5d2
Thibaut Sardan <github@thib.top>
2022-05-20 15:35:56 +0200

    add tests for hidden accounts in the auth view

--- packages/extension-ui/src/Popup/Authorize/Authorize.spec.tsx
+++ packages/extension-ui/src/Popup/Authorize/Authorize.spec.tsx
@@ -13,23 +13,41 @@ import { ThemeProvider } from 'styled-components';
 import { AccountContext, AuthorizeReqContext, themes, Warning } from '../../components';
 import { Header } from '../../partials';
 import { buildHierarchy } from '../../util/buildHierarchy';
+import Account from '../Accounts/Account';
 import Request from './Request';
 import Authorize from '.';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
 configure({ adapter: new Adapter() });
 
-const accounts = [
+const oneRequest = [{ id: '1', request: { origin: '???' }, url: 'http://polkadot.org' }];
+
+const twoRequests = [
+  ...oneRequest,
+  { id: '2', request: { origin: 'abc' }, url: 'http://polkadot.pl' }
+];
+
+const oneAccount = [
   { address: '5FjgD3Ns2UpnHJPVeRViMhCttuemaRXEqaD8V5z4vxcsUByA', name: 'A', type: 'sr25519' }
+] as AccountJson[];
+
+const twoAccountsOnehidden = [
+  ...oneAccount,
+  { address: '5GYmFzQCuC5u3tQNiMZNbFGakrz3Jq31NmMg4D2QAkSoQ2g5', isHidden: true, name: 'B', type: 'sr25519' }
+] as AccountJson[];
+
+const threeAccountsOnehidden = [
+  ...twoAccountsOnehidden,
+  { address: '5D2TPhGEy2FhznvzaNYW9AkuMBbg3cyRemnPsBvBY4ZhkZXA', name: 'BB', parentAddress: twoAccountsOnehidden[1].address, type: 'sr25519' }
 ] as AccountJson[];
 
 describe('Authorize', () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  const mountAuthorize = (authorizeRequests: AuthorizeRequest[] = [], withAccounts = true): ReactWrapper => mount(
+  const mountAuthorize = (authorizeRequests: AuthorizeRequest[] = [], accounts: AccountJson[] = oneAccount): ReactWrapper => mount(
     <AuthorizeReqContext.Provider value={authorizeRequests}>
       <AccountContext.Provider
         value={{
-          accounts: withAccounts ? accounts : [],
+          accounts,
           hierarchy: accounts ? buildHierarchy(accounts) : []
         }}
       >
@@ -47,17 +65,14 @@ describe('Authorize', () => {
   });
 
   it('render requests', () => {
-    const wrapper = mountAuthorize([{ id: '1', request: { origin: '???' }, url: 'http://polkadot.org' }]);
+    const wrapper = mountAuthorize(oneRequest);
 
     expect(wrapper.find(Request).length).toBe(1);
     expect(wrapper.find(Request).find('.warning-message').text()).toBe('An application, self-identifying as ??? is requesting access from http://polkadot.org');
   });
 
   it('render more request but just one accept button', () => {
-    const wrapper = mountAuthorize([
-      { id: '1', request: { origin: '???' }, url: 'http://polkadot.org' },
-      { id: '2', request: { origin: 'abc' }, url: 'http://polkadot.pl' }
-    ]);
+    const wrapper = mountAuthorize(twoRequests);
 
     expect(wrapper.find(Request).length).toBe(2);
     expect(wrapper.find(Warning).length).toBe(2);
@@ -65,11 +80,33 @@ describe('Authorize', () => {
     expect(wrapper.find('button.acceptButton').length).toBe(1);
   });
 
-  it.only('render a warning and explication text when there is no account', () => {
-    const wrapper = mountAuthorize([{ id: '1', request: { origin: '???' }, url: 'http://polkadot.org' }], false);
+  it('render a warning and explication text when there is no account', () => {
+    const wrapper = mountAuthorize(oneRequest, []);
 
     expect(wrapper.find(Request).length).toBe(1);
     expect(wrapper.find(Request).find('.warning-message').text()).toBe("You do not have any account. Please create an account and refresh the application's page.");
     expect(wrapper.find('button.acceptButton').length).toBe(1);
+  });
+
+  it('show the right amount of accounts', () => {
+    const wrapper = mountAuthorize(oneRequest);
+
+    expect(wrapper.find(Request).length).toBe(1);
+    expect(wrapper.find(Account).length).toBe(1);
+    expect(wrapper.find('button.acceptButton').length).toBe(1);
+  });
+
+  it('does not show the hidden accounts', () => {
+    const wrapper = mountAuthorize(oneRequest, twoAccountsOnehidden);
+
+    expect(wrapper.find(Request).length).toBe(1);
+    expect(wrapper.find(Account).length).toBe(1);
+  });
+
+  it('shows the children of hidden accounts', () => {
+    const wrapper = mountAuthorize(oneRequest, threeAccountsOnehidden);
+
+    expect(wrapper.find(Request).length).toBe(1);
+    expect(wrapper.find(Account).length).toBe(2);
   });
 });

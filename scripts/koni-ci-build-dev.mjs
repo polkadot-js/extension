2e06e8d7694d9c03f4b0effcbabc638816eb228d
AnhMTV <maithachvietanh@gmail.com>
2022-05-24 15:13:24 +0700

    Fix dev build action if file bigger than 10MB by upload to cloud

--- scripts/koni-ci-build-dev.mjs
+++ scripts/koni-ci-build-dev.mjs
@@ -2,8 +2,9 @@
 // Copyright 2017-2022 @polkadot/dev authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import fs from 'fs';
 import path from 'path';
+import axios from 'axios';
+import fs from "fs";
 
 import execSync from '@polkadot/dev/scripts/execSync.mjs';
 import {Webhook} from "discord-webhook-node";
@@ -12,37 +13,59 @@ console.log('$ polkadot-ci-ghact-build', process.argv.slice(2).join(' '));
 
 const discordHook = new Webhook(process.env.DISCORD_WEBHOOK);
 
-function runClean () {
+function runClean() {
   execSync('yarn polkadot-dev-clean-build');
 }
 
-function runCheck () {
+function runCheck() {
   execSync('yarn lint');
 }
 
-function runTest () {
+function runTest() {
   execSync('yarn test');
 }
 
-function runBuild () {
+function runBuild() {
   execSync('yarn build');
 }
 
-function npmGetVersion () {
-  return JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8')
-  ).version;
+function npmGetVersion() {
+  return JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8')).version;
 }
 
-function uploadBuild(){
+async function uploadBuild() {
   const refName = process.env.REF_NAME
   const commitMessage = process.env.COMMIT_MESSAGE
   const nowTimestamp = new Date().getTime()
   const sRefName = refName.replace(/(\/)/g, '-');
-  const newName = `./${sRefName}-build-${npmGetVersion()}-${nowTimestamp}.zip`
-  fs.renameSync('./master-build.zip', newName)
-  discordHook.send('Finish build ' + refName + ': '+ commitMessage)
-  discordHook.sendFile(newName)
+
+  discordHook.send('Finish build ' + refName + ': ' + commitMessage)
+
+  try {
+    const cloudConfig = JSON.parse(process.env.NEXTCLOUD_CONFIG)
+    const {nextCloudUrl, nextCloudUsername, nextCloudPassword, folder, shareFolder} = cloudConfig;
+
+    const newName = `./${sRefName}-build-${npmGetVersion()}-${nowTimestamp}.zip`
+    const downloadLink = `${nextCloudUrl}/s/${shareFolder}/download?path=%2F&files=${newName}`;
+    const uploadUrl = `${nextCloudUrl}/remote.php/dav/files/${nextCloudUsername}/${folder}/${newName}`;
+
+    const file = await fs.readFileSync('./master-build.zip');
+
+    const rs = await axios.put(uploadUrl, file, {
+      auth: {
+        username: nextCloudUsername, password: nextCloudPassword
+      }, headers: {'Content-Type': 'text/octet-stream'}
+    })
+
+    if (rs.statusText === 'Created') {
+      discordHook.send('Upload success! Please download here:')
+      discordHook.send(downloadLink)
+    } else {
+      console.warn('Can not upload build to discord!')
+    }
+  } catch (e) {
+    console.warn('NEXTCLOUD_CONFIG WRONG', e)
+  }
 }
 
 runClean();

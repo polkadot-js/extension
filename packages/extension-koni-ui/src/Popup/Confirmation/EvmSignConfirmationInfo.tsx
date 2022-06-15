5f18331c3a689aba9ded54123d34fc2c4061dd40
AnhMTV <maithachvietanh@gmail.com>
2022-06-16 00:18:10 +0700

    Improve sign message popup

--- packages/extension-koni-ui/src/Popup/Confirmation/EvmSignConfirmationInfo.tsx
+++ packages/extension-koni-ui/src/Popup/Confirmation/EvmSignConfirmationInfo.tsx
@@ -4,7 +4,7 @@
 import { ConfirmationsQueue } from '@subwallet/extension-base/background/KoniTypes';
 import useTranslation from '@subwallet/extension-koni-ui/hooks/useTranslation';
 import { ThemeProps } from '@subwallet/extension-koni-ui/types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 interface Props extends ThemeProps {
@@ -14,24 +14,48 @@ interface Props extends ThemeProps {
 
 function EvmSignConfirmationInfo ({ className, confirmation: { payload } }: Props): React.ReactElement {
   const { t } = useTranslation();
-  const transaction = payload;
+  const [signMethod, setSignMethod] = useState('');
+  const [rawData, setRawData] = useState<string>('');
+  const [warning, setWarning] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (payload.type === 'eth_sign') {
+      setWarning('Signing this message can be dangerous. This signature could potentially perform any operation on your account\'s behalf, including granting complete control of your account and all of its assets to the requesting site. Only sign this message if you know what you\'re doing or completely trust the requesting site.');
+      setSignMethod('ETH Sign');
+    } else if (payload.type === 'personal_sign') {
+      setSignMethod('Personal Sign');
+    } else if (payload.type === 'eth_signTypedData') {
+      setSignMethod('Sign Typed Data');
+    } else if (payload.type === 'eth_signTypedData_v1') {
+      setSignMethod('Sign Typed Data V1');
+    } else if (payload.type === 'eth_signTypedData_v3') {
+      setSignMethod('Sign Typed Data V3');
+    } else if (payload.type === 'eth_signTypedData_v4') {
+      setSignMethod('Sign Typed Data V4');
+    }
+
+    const raw = typeof payload.payload === 'string' ? payload.payload : JSON.stringify(payload.payload, null, 2);
+
+    setRawData(raw);
+  }, [payload]);
 
   return <div className={className}>
     <div className='signature-wrapper'>
       <div>
-        <span className='label'>{t<string>('Type')}</span><span className='value'>{transaction.type}</span>
+        <span className='label'>{t<string>('Sign Method')}</span><span className='value'>{signMethod}</span>
       </div>
+      {warning && <div className='value warning-message'>{warning}</div>}
       <div>
-        <span className='label'>{t<string>('Raw')}</span><span className='pre-wrap'>
-          {typeof transaction.payload === 'string' ? transaction.payload : JSON.stringify(transaction.payload, null, 2)}
-        </span>
+        <span className='label'>{t<string>('Raw Data')}</span>
+        <div className='pre-wrap'>{rawData}</div>
       </div>
     </div>
   </div>;
 }
 
-export default styled(EvmSignConfirmationInfo)(({ theme }: Props) => `  
+export default styled(EvmSignConfirmationInfo)(({ theme }: Props) => `
   .signature-wrapper {
+    position: relative;
     width: 100%;  
   }
   
@@ -39,13 +63,15 @@ export default styled(EvmSignConfirmationInfo)(({ theme }: Props) => `
     font-weight: bold;
     padding-right: 8px;
   }
-  .value {
-    color: #7B8098;
+  .warning-message {
+    color: red;
   }
   
   .pre-wrap {
     color: #7B8098;
     display: block;
     white-space: pre-wrap;
+    width: 100%;
+    overflow: auto;
   }
 `);

7e7c668172f5fcaa3de1b967937e61a79173f74c
khainh <huukhai0510@gmail.com>
2022-06-22 18:00:00 +0700

    Update test.spec.ts

--- packages/extension-koni-base/src/api/nft/unique_nft/test.spec.ts
+++ packages/extension-koni-base/src/api/nft/unique_nft/test.spec.ts
@@ -1,6 +1,7 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { NftCollection, NftItem } from '@subwallet/extension-base/background/KoniTypes';
 import { UniqueNftApiV2 } from '@subwallet/extension-koni-base/api/nft/unique_nft/uniqueNftV2';
 
 jest.setTimeout(50000);
@@ -12,15 +13,19 @@ describe('test DotSama APIs', () => {
     nftApi.setChain('uniqueNft');
     nftApi.setAddresses(['5HdwtP77HPPVzCVqEu38GGCHf1g9eQXCMZV5aPhZttN3sKF4']);
 
-    await nftApi.fetchNfts(
-      (data) => {
+    await nftApi.fetchNfts({
+      updateItem: (data: NftItem) => {
         console.log(data);
       },
-      (data) => {
+      updateCollection: (data: NftCollection) => {
         console.log(data);
       },
-      (ready) => {
+      updateReady: (ready) => {
         console.log(ready);
-      });
+      },
+      updateNftIds: (networkKey: string) => {
+        console.log(networkKey);
+      }
+    });
   });
 });

7a8ea1d3d4b52466626a4c90696f9fe68023cfe7
AnhMTV <maithachvietanh@gmail.com>
2022-07-13 12:50:25 +0700

    Fix wrong button text color in welcome screen

--- packages/extension-koni-ui/src/Popup/Accounts/AddAccount.tsx
+++ packages/extension-koni-ui/src/Popup/Accounts/AddAccount.tsx
@@ -112,18 +112,18 @@ export default React.memo(styled(AddAccount)(({ theme }: Props) => `
 
   .create-account {
     background-color: ${theme.buttonBackground2};
-    color: ${theme.buttonTextColor3};
+    color: ${theme.buttonTextColor};
   }
 
   .add-account-link {
     justify-content: center;
-    color: ${theme.textColor};
+    color: ${theme.buttonTextColor};
     opacity: 1;
   }
 
   .add-account-link__create-account {
     justify-content: center;
-    color: ${theme.buttonTextColor3};
+    color: ${theme.buttonTextColor};
     opacity: 1;
   }
 

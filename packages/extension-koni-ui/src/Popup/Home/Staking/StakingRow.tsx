d31598dd3191aa008a75879da0db3757c0f95f8b
Hieu Dao <daodinhhieu@gmail.com>
2022-07-29 01:49:16 +0700

    Update staking logo
    Update staking logo

--- packages/extension-koni-ui/src/Popup/Home/Staking/StakingRow.tsx
+++ packages/extension-koni-ui/src/Popup/Home/Staking/StakingRow.tsx
@@ -317,7 +317,7 @@ export default React.memo(styled(StakingRow)(({ theme }: Props) => `
 
   .network-logo {
     display: block;
-    min-width: 32px;
+    min-width: 36px;
     height: 36px;
     border-radius: 100%;
     overflow: hidden;

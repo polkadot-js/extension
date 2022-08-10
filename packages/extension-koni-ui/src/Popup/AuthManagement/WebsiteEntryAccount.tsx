dc465e5a6c3608821d1d4ce60da292e633beb564
S2kael <laiducminh1002@gmail.com>
2022-08-10 13:36:03 +0700

    [Issue-518] add saveCurrentAccountAddress trigger after change auth site

--- packages/extension-koni-ui/src/Popup/AuthManagement/WebsiteEntryAccount.tsx
+++ packages/extension-koni-ui/src/Popup/AuthManagement/WebsiteEntryAccount.tsx
@@ -6,8 +6,11 @@ import type { ThemeProps } from '../../types';
 import { AuthUrls } from '@subwallet/extension-base/background/handlers/State';
 import { AccountInfoEl } from '@subwallet/extension-koni-ui/components';
 import Toggle from '@subwallet/extension-koni-ui/components/Toggle';
-import { changeAuthorizationPerAcc } from '@subwallet/extension-koni-ui/messaging';
+import { changeAuthorizationPerAcc, saveCurrentAccountAddress } from '@subwallet/extension-koni-ui/messaging';
+import { RootState } from '@subwallet/extension-koni-ui/stores';
+import { noop } from '@subwallet/extension-koni-ui/util/function';
 import React, { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 interface Props extends ThemeProps {
@@ -19,6 +22,8 @@ interface Props extends ThemeProps {
 }
 
 function WebsiteEntryAccount ({ address, className = '', isConnected, setList, url }: Props): React.ReactElement<Props> {
+  const { currentAccount } = useSelector((state: RootState) => state);
+
   const [isAccountConnected, setAccountConnected] = useState<boolean>(false);
 
   useEffect(() => {
@@ -29,8 +34,17 @@ function WebsiteEntryAccount ({ address, className = '', isConnected, setList, u
     changeAuthorizationPerAcc(address, !isConnected, url, (data) => {
       setList(data);
       setAccountConnected(!isConnected);
-    }).catch(console.error);
-  }, [address, isConnected, setList, url]);
+    })
+      .then(() => {
+        if (currentAccount.account?.address) {
+          saveCurrentAccountAddress({ address: currentAccount.account.address }, noop)
+            .catch((e) => {
+              console.error('There is a problem when trigger saveCurrentAccountAddress', e);
+            });
+        }
+      })
+      .catch(console.error);
+  }, [address, currentAccount.account?.address, isConnected, setList, url]);
 
   return (
     <div className={className}>

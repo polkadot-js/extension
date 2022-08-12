5026eaf332aa78cc50d5d2a6f6128c04f595429e
NamPhamc99 <namphamc99@gmail.com>
2022-08-12 17:35:16 +0700

    testing oak-js sdk

--- packages/extension-koni-base/src/api/bonding/ignore-para-bonding.spec.ts
+++ packages/extension-koni-base/src/api/bonding/ignore-para-bonding.spec.ts
@@ -4,6 +4,8 @@
 import { PREDEFINED_NETWORKS } from '@subwallet/extension-koni-base/api/predefinedNetworks';
 import { DOTSAMA_AUTO_CONNECT_MS } from '@subwallet/extension-koni-base/constants';
 import { getCurrentProvider } from '@subwallet/extension-koni-base/utils';
+// @ts-ignore
+import { Scheduler, oakConstants } from 'oak-js-library/dist';
 
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
@@ -204,13 +206,17 @@ describe('test DotSama APIs', () => {
   });
 
   test('get turing auto-compounding APY', async () => {
-    const provider = new WsProvider(getCurrentProvider(PREDEFINED_NETWORKS.turingStaging), DOTSAMA_AUTO_CONNECT_MS);
-    const api = new ApiPromise({ provider });
-    const apiPromise = await api.isReady;
+    // const provider = new WsProvider(getCurrentProvider(PREDEFINED_NETWORKS.turingStaging), DOTSAMA_AUTO_CONNECT_MS);
+    // const api = new ApiPromise({ provider });
+    // const apiPromise = await api.isReady;
+    //
+    // const resp = apiPromise.tx.automationTime.scheduleAutoCompoundDelegatedStakeTask('1658854800', '172800', '691Fmzb8rhYmBxLvaqYEUApK22s3o6eCzC4whDY7dZZ83YYQ', '10000000000');
+    // const paymentInfo = await resp.paymentInfo('5HbcGs2QXVAc6Q6eoTzLYNAJWpN17AkCFRLnWDaHCiGYXvNc');
+    //
+    // console.log(paymentInfo.toHuman()); // might or might not be right
 
-    const resp = apiPromise.tx.automationTime.scheduleAutoCompoundDelegatedStakeTask('1658854800', '172800', '691Fmzb8rhYmBxLvaqYEUApK22s3o6eCzC4whDY7dZZ83YYQ', '10000000000');
-    const paymentInfo = await resp.paymentInfo('5HbcGs2QXVAc6Q6eoTzLYNAJWpN17AkCFRLnWDaHCiGYXvNc');
+    const scheduler = new Scheduler(oakConstants.OakChains.TUR);
 
-    console.log(paymentInfo.toHuman()); // might or might not be right
+    console.log(scheduler);
   });
 });

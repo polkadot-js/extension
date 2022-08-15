4296d552070ca88a0efc2a1d1603c2d7caa6d5b6
Nam Phạm <namphamc99@gmail.com>
2022-08-15 13:50:49 +0700

    fix bug React memory leak

--- packages/extension-koni-ui/src/Popup/Home/Staking/StakingContainer.tsx
+++ packages/extension-koni-ui/src/Popup/Home/Staking/StakingContainer.tsx
@@ -62,6 +62,10 @@ function StakingContainer ({ className, data, loading, priceMap, stakeUnlockingT
   useEffect(() => {
     handlerResize();
     window.addEventListener('resize', handlerResize);
+
+    return () => {
+      window.removeEventListener('resize', handlerResize);
+    };
   }, []);
 
   const getScrollbarWidth = () => {
@@ -86,11 +90,23 @@ function StakingContainer ({ className, data, loading, priceMap, stakeUnlockingT
   };
 
   useEffect(() => {
-    getScrollbarWidth();
+    let isMounted = true;
+
+    if (isMounted) {
+      getScrollbarWidth();
+    }
+
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
   useEffect(() => {
     setListWidth(containerWidth - scrollWidth);
+
+    return () => {
+      setListWidth(0);
+    };
   }, [containerWidth, scrollWidth]);
 
   const handleNavigateBonding = useCallback(() => {

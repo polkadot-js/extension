bb6350ff9ceee1fdcb5f2ff990e4828addc702ab
S2kael <laiducminh1002@gmail.com>
2022-08-29 17:05:00 +0700

    [Issue-585] [Balance] [Decimal] Rollback rule

--- packages/extension-koni-ui/src/components/Balance/index.tsx
+++ packages/extension-koni-ui/src/components/Balance/index.tsx
@@ -21,7 +21,11 @@ const BalanceValComponent = ({ className, newRule = true, startWithSymbol = fals
   let [prefix, postfix] = typeof value === 'object' ? value.toFormat(9).split('.') : value.toString().split('.');
   const length = newRule ? 4 : 4;
 
-  postfix = postfix?.substring(0, length);
+  if (startWithSymbol) {
+    postfix = postfix?.substring(0, length - 1);
+  } else {
+    postfix = postfix?.substring(0, length);
+  }
 
   const lastSymbol = postfix?.slice(-1);
   const isString = /^[KMB]/.test(lastSymbol);

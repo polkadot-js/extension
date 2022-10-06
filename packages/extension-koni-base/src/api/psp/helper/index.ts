2e74e5e6502d034c90bbe8091d4a260ce9ff6b87
Nam Phạm <namphamc99@gmail.com>
2022-10-06 12:00:47 +0700

    testing PSP tokens

---
+++ packages/extension-koni-base/src/api/psp/helper/index.ts
@@ -0,0 +1,5 @@
+// Copyright 2019-2022 @subwallet/extension-koni authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-unsafe-assignment
+export const PSP22Contract: Record<string, any> = require('./psp22_abi.json');
--- packages/extension-koni-base/src/api/psp/ignore-psp-test.spec.ts
+++ packages/extension-koni-base/src/api/psp/ignore-psp-test.spec.ts
@@ -2,19 +2,106 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { PREDEFINED_NETWORKS } from '@subwallet/extension-koni-base/api/predefinedNetworks';
+import { PSP22Contract } from '@subwallet/extension-koni-base/api/psp/helper';
 import { DOTSAMA_AUTO_CONNECT_MS } from '@subwallet/extension-koni-base/constants';
 import { getCurrentProvider } from '@subwallet/extension-koni-base/utils';
 
 import { ApiPromise, WsProvider } from '@polkadot/api';
+import { ContractPromise } from '@polkadot/api-contract';
+import { Weight } from '@polkadot/types/interfaces';
+import { BN, BN_MILLION, BN_TEN, BN_THOUSAND, BN_TWO, bnMin } from '@polkadot/util';
 
 jest.setTimeout(5000000);
 
-describe('test DotSama APIs', () => {
-  test('test get Validator', async () => {
-    const provider = new WsProvider(getCurrentProvider(PREDEFINED_NETWORKS.alephTest), DOTSAMA_AUTO_CONNECT_MS);
-    const api = new ApiPromise({ provider });
-    const apiPromise = await api.isReady;
+// Some chains incorrectly use these, i.e. it is set to values such as 0 or even 2
+// Use a low minimum validity threshold to check these against
+const THRESHOLD = BN_THOUSAND.div(BN_TWO);
+const DEFAULT_TIME = new BN(6_000);
 
-    apiPromise.
+function getBlockInterval (api: ApiPromise): BN {
+  const aDay = new BN(24 * 60 * 60 * 1000);
+
+  // @ts-ignore
+  return bnMin(aDay, (
+    // Babe, e.g. Relay chains (Substrate defaults)
+    api.consts.babe?.expectedBlockTime ||
+    // POW, eg. Kulupu
+    api.consts.difficulty?.targetBlockTime ||
+    // Subspace
+    api.consts.subspace?.expectedBlockTime || (
+      // Check against threshold to determine value validity
+      // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      api.consts.timestamp?.minimumPeriod.gte(THRESHOLD)
+        // Default minimum period config
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        ? api.consts.timestamp.minimumPeriod.mul(BN_TWO)
+        : api.query.parachainSystem
+          // default guess for a parachain
+          ? DEFAULT_TIME.mul(BN_TWO)
+          // default guess for others
+          : DEFAULT_TIME
+    )
+  ));
+}
+
+function getMegaGas (api: ApiPromise): BN {
+  const maxBlockWeight = api.consts.system.blockWeights
+    // @ts-ignore
+    ? api.consts.system.blockWeights.maxBlock as BN
+    : api.consts.system.maximumBlockWeight as Weight;
+
+  return maxBlockWeight.div(BN_MILLION).div(BN_TEN);
+}
+
+describe('test DotSama APIs', () => {
+  test('test smart contract', async () => {
+    const provider = new WsProvider(getCurrentProvider(PREDEFINED_NETWORKS.alephTest), DOTSAMA_AUTO_CONNECT_MS);
+    const apiPromise = new ApiPromise({ provider });
+    const api = await apiPromise.isReady;
+
+    const contract = new ContractPromise(api, PSP22Contract, '5CY8zDBjUDNwZBHdGbERtLLSZqY7dJYsm1KhY6tSorYvnSke');
+
+    // const totalSupply = await contract.query['psp22::totalSupply']('5HbcGs2QXVAc6Q6eoTzLYNAJWpN17AkCFRLnWDaHCiGYXvNc', { gasLimit: -1 });
+    //
+    // const _result = totalSupply.output.toHuman();
+    //
+    // console.log(_result);
+    //
+    // const balanceOf = await contract.query['psp22::balanceOf']('5HbcGs2QXVAc6Q6eoTzLYNAJWpN17AkCFRLnWDaHCiGYXvNc', { gasLimit: -1 }, '5HbcGs2QXVAc6Q6eoTzLYNAJWpN17AkCFRLnWDaHCiGYXvNc');
+    //
+    // const result = balanceOf.output.toHuman();
+    //
+    // console.log(balanceOf.result.isOk);
+    //
+    // console.log(result);
+    //
+    const transfer = contract.tx['psp22::transfer']({ gasLimit: '100000' }, '5Dy4D7r9HeWvrcyjUF51zK3z3gePYrcaT9fF8Q7M6LYhgrGM', '100000', '');
+
+    console.log((await transfer.paymentInfo('5Dy4D7r9HeWvrcyjUF51zK3z3gePYrcaT9fF8Q7M6LYhgrGM')).toHuman());
+
+    const megaGas = getMegaGas(api);
+
+    const blockInterval = getBlockInterval(api);
+
+    console.log(blockInterval.toString());
+
+    const weight = megaGas.mul(BN_MILLION);
+
+    const executionTime = weight.mul(blockInterval).div(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      api.consts.system.blockWeights
+        // @ts-ignore
+        ? api.consts.system.blockWeights.maxBlock
+        : api.consts.system.maximumBlockWeight as Weight
+    ).toNumber();
+
+    console.log('megaGas', megaGas.toNumber());
+    console.log('weight', weight.toNumber());
+    console.log(executionTime);
+
+    // TODO: calculate gasLimit, gasFee
+    // TODO: UI
   });
 });

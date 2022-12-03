d66489b8c030159eb1eb706ec296ed6f90e27335
S2kael <laiducminh1002@gmail.com>
2022-12-03 17:47:41 +0700

    [Issue-274] Update message network not active

--- packages/extension-koni-ui/src/contexts/ScannerContext.tsx
+++ packages/extension-koni-ui/src/contexts/ScannerContext.tsx
@@ -356,7 +356,7 @@ export function ScannerContextProvider ({ children }: ScannerContextProviderProp
     }
 
     if (!senderNetwork.active) {
-      throw new Error('Signing Error: Network is not active.');
+      throw new Error(`Signing Error: Network ${senderNetwork.chain?.replace(' Relay Chain', '')} is not active.`);
     }
 
     if (!sender) {

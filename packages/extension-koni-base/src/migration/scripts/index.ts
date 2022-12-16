076ac6340ebe5e9e736b79cffb6c1b55ec998ecd
AnhMTV <maithachvietanh@gmail.com>
2022-12-16 15:28:03 +0700

    Fix eslint problem

--- packages/extension-koni-base/src/migration/scripts/index.ts
+++ packages/extension-koni-base/src/migration/scripts/index.ts
@@ -3,12 +3,11 @@
 
 import ChangeRouteToHome from '@subwallet/extension-koni-base/migration/scripts/ChangeRouteToHome';
 import RemoveWrongCrowdloan from '@subwallet/extension-koni-base/migration/scripts/RemoveWrongCrowdloan';
+import ResetTransactionHistoryEventIdx from '@subwallet/extension-koni-base/migration/scripts/ResetTransactionHistoryEventIdx';
 
 import BaseMigrationJob from '../Base';
 import ClearOldStorage from './ClearOldStorage';
 import ConvertTransactionHistoryFromChromeStorageToIndexedDB from './ConvertTransactionHistoryFromChromeStorageToIndexedDB';
-import ResetTransactionHistoryEventIdx
-  from "@subwallet/extension-koni-base/migration/scripts/ResetTransactionHistoryEventIdx";
 
 export default <Record<string, typeof BaseMigrationJob>> {
   // '0.5.3-2': FixMissingTransactionHistory,

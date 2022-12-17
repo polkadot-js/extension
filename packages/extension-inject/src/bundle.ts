45a02f726815bee45ad2094c799219e01fa18d82
Jaco <jacogr@gmail.com>
2022-12-17 14:38:32 +0200

    connect(<dappName>) new-style support (#1175)
    * connect(<dappName>) new-style support
    
    * Add connect interface
    
    * Seed required
    
    * Support both old & new codepaths in inject
    
    * Explicit enable(...) reject on new-style
    
    * Cleanups
    
    * Condense enable(...) error
    
    * Adjust enable ordering
    
    * Adjust web3IsInjected

--- packages/extension-inject/src/bundle.ts
+++ packages/extension-inject/src/bundle.ts
@@ -1,9 +1,18 @@
 // Copyright 2019-2022 @polkadot/extension-inject authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Injected, InjectedWindow, InjectOptions } from './types';
+import type { Injected, InjectedExtension, InjectedWindow, InjectOptions } from './types';
+
+import { cyrb53 } from './cyrb53';
 
 export { packageInfo } from './packageInfo';
+
+// setting for new-style connect (more-secure with no details exposed without
+// user acknowledgement, however slightly less-compatible with all dapps, some
+// may have not upgraded and don't have access to the latest interfaces)
+//
+// NOTE: In future versions this will be made the default
+const IS_CONNECT_CAPABLE = false;
 
 // It is recommended to always use the function below to shield the extension and dapp from
 // any future changes. The exposed interface will manage access between the 2 environments,
@@ -15,10 +24,24 @@ export function injectExtension (enable: (origin: string) => Promise<Injected>, 
   // don't clobber the existing object, we will add it (or create as needed)
   windowInject.injectedWeb3 = windowInject.injectedWeb3 || {};
 
-  // add our enable function
-  windowInject.injectedWeb3[name] = {
-    enable: (origin: string): Promise<Injected> =>
-      enable(origin),
-    version
-  };
+  if (IS_CONNECT_CAPABLE) {
+    // expose our extension on the window object, new-style with connect(origin)
+    windowInject.injectedWeb3[cyrb53(`${name}/${version}`)] = {
+      connect: (origin: string): Promise<InjectedExtension> =>
+        enable(origin).then(({ accounts, metadata, provider, signer }) => ({
+          accounts, metadata, name, provider, signer, version
+        })),
+      enable: (): Promise<Injected> =>
+        Promise.reject(
+          new Error('This extension does not have support for enable(...), rather is only supports the new connect(...) variant (no extension name/version metadata without specific user-approval)')
+        )
+    };
+  } else {
+    // expose our extension on the window object, old-style with enable(origin)
+    windowInject.injectedWeb3[name] = {
+      enable: (origin: string): Promise<Injected> =>
+        enable(origin),
+      version
+    };
+  }
 }
---
+++ packages/extension-inject/src/cyrb53.ts
@@ -0,0 +1,33 @@
+// Copyright 2019-2022 @polkadot/extension-inject authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// cyrb53 (c) 2018 bryc (github.com/bryc)
+// A fast and simple hash function with decent collision resistance.
+// Largely inspired by MurmurHash2/3, but with a focus on speed/simplicity.
+// Public domain. Attribution appreciated.
+//
+// From https://github.com/bryc/code/blob/fed42df9db547493452e32375c93d7854383e480/jshash/experimental/cyrb53.js
+// As shared in https://stackoverflow.com/a/52171480
+//
+// Small changes made to the code as linked above:
+// . - Seed value is required (set  as Date.now() in usage, could change)
+//   - Return value is a hex string (as per comment in SO answer)
+//   - TS typings added
+//   - Non-intrusive coding-style variable declaration changes
+export function cyrb53 (input: string, seed = Date.now()): string {
+  let h1 = 0xdeadbeef ^ seed;
+  let h2 = 0x41c6ce57 ^ seed;
+
+  for (let i = 0; i < input.length; i++) {
+    const ch = input.charCodeAt(i);
+
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  // https://stackoverflow.com/a/52171480
+  return (h2 >>> 0).toString(16).padStart(8, '0') + (h1 >>> 0).toString(16).padStart(8, '0');
+}

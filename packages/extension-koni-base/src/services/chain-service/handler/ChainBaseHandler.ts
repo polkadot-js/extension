1514cc4faf28b672bf018ab15cc60015a17a5dff
Nam Phạm <namphamc99@gmail.com>
2022-12-22 09:31:46 +0700

    remove BaseChainHandler

--- packages/extension-koni-base/src/services/chain-service/handler/ChainBaseHandler.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2019-2022 @subwallet/extension-koni-base authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export abstract class ChainBaseHandler {
-  abstract initApi();
-}

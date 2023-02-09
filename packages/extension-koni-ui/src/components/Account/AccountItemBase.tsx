7e44998b2310bfbee07f9c4a1e26d8fa5c3801f3
dominhquang <quangdm@subwallet.app>
2023-02-09 18:31:12 +0700

    [Issue-1020]: Upgrade UI - Update AccountCard and AccountItem and SettingItem

--- packages/extension-koni-ui/src/components/Account/AccountItemBase.tsx
+++ packages/extension-koni-ui/src/components/Account/AccountItemBase.tsx
@@ -4,29 +4,28 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@subwallet/extension-koni-ui/stores';
-import { AccountJson } from '@subwallet/extension-base/background/types';
 import { _getSubstrateGenesisHash } from '@subwallet/extension-base/services/chain-service/utils';
 import { _ChainInfo } from '@subwallet/chain-list/types';
 import { accountAllRecoded, defaultRecoded, recodeAddress } from '@subwallet/extension-koni-ui/util';
 import { Recoded } from '@subwallet/extension-koni-ui/types';
 import { isAccountAll } from '@subwallet/extension-koni-base/utils';
-import AccountItem from '@subwallet/react-ui/es/web3-block/account-item';
+import AccountItem, { AccountItemProps } from '@subwallet/react-ui/es/web3-block/account-item';
 import { isEthereumAddress } from '@polkadot/util-crypto';
-import AccountCard from '@subwallet/react-ui/es/web3-block/account-card';
+import { KeypairType } from '@polkadot/util-crypto/types';
 
-interface AccountItemProps extends AccountJson {
-  itemType?: 'card' | 'item' | 'item-without-name';
+export interface _AccountItemProps extends AccountItemProps {
   className?: string;
-  avatarSize?: number;
-  isSelected?: boolean;
-  onPressItem?: () => void;
+  genesisHash?: string | null;
+  type?: KeypairType;
+  accountName?: string;
 }
 
-function SwAccountItem ({ className, avatarSize, address, genesisHash, type: givenType, isSelected, name, itemType, onPressItem }: AccountItemProps): React.ReactElement<AccountItemProps> {
+function AccountItemBase (props: Partial<_AccountItemProps>): React.ReactElement<Partial<_AccountItemProps>> {
   const { accounts } = useSelector((state: RootState) => state.accountState);
   const { chainInfoMap } = useSelector((state: RootState) => state.chainStore);
   const [{ genesisHash: recodedGenesis, prefix }, setRecoded] = useState<Recoded>(defaultRecoded);
-
+  const { address, type: givenType } = props;
+  const _isAccountAll = address && isAccountAll(address);
   const getChainInfoByGenesisHash = useCallback((hash?: string | null): _ChainInfo | null => {
     if (!hash) {
       return null;
@@ -46,8 +45,7 @@ function SwAccountItem ({ className, avatarSize, address, genesisHash, type: giv
 
     return null;
   }, [chainInfoMap]);
-  const _isAccountAll = address && isAccountAll(address);
-  const networkInfo = getChainInfoByGenesisHash(genesisHash || recodedGenesis);
+  const networkInfo = getChainInfoByGenesisHash(props.genesisHash || recodedGenesis);
   const iconTheme = useMemo((): 'polkadot'|'ethereum' => {
     if (!address) {
       return 'polkadot';
@@ -77,48 +75,15 @@ function SwAccountItem ({ className, avatarSize, address, genesisHash, type: giv
     //TODO: change recoded
   }, [accounts, _isAccountAll, address, networkInfo, givenType]);
 
-  if (itemType === 'card') {
-    return (
-      <AccountCard
-        address={address}
-        avatarIdentPrefix={prefix || 42}
-        accountName={name || ''}
-        isSelected={isSelected}
-        onClick={onPressItem}
-      />
-    );
-  }
-
-  if (itemType === 'item') {
-    return (
-      <AccountItem
-        address={address}
-        avatarIdentPrefix={prefix || 42}
-        isSelected={isSelected}
-        onClick={onPressItem}
-      />
-    );
-  }
-
   return (
     <AccountItem
-      className={className}
-      avatarSize={avatarSize}
-      address={address}
+      {...props}
+      address={address || ''}
+      className={props.className}
       avatarIdentPrefix={prefix || 42}
       avatarTheme={iconTheme}
-      addressPreLength={4}
-      addressSufLength={4}
-      onClick={onPressItem}
-      isSelected={isSelected}
-      renderMiddleItem={(x) => (
-        <div className='account-item-content-wrapper'>
-          <div className={'account-item-name'}>{name}</div>
-          <div className={'account-item-address-wrapper'}>({x})</div>
-        </div>
-      )}
     />
   );
 }
 
-export default SwAccountItem;
+export default AccountItemBase;

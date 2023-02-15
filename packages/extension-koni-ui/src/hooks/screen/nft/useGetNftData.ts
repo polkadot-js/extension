f6608d8b24b265f01dee135c234d33211d754daa
AnhMTV <maithachvietanh@gmail.com>
2023-02-15 12:22:47 +0700

    Fix lint after merge

--- packages/extension-koni-ui/src/hooks/screen/nft/useGetNftData.ts
+++ packages/extension-koni-ui/src/hooks/screen/nft/useGetNftData.ts
@@ -1,11 +1,9 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {useSelector} from "react-redux";
-import {RootState} from "@subwallet/extension-koni-ui/stores";
+import { RootState } from '@subwallet/extension-koni-ui/stores';
+import { useSelector } from 'react-redux';
 
 export default function useGetNftData () {
-  const {nftCollections, nftItems} = useSelector((state: RootState) => state.nft);
-
-
+  const { nftCollections, nftItems } = useSelector((state: RootState) => state.nft);
 }

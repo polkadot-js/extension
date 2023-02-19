42ba1126999375a45d82e808039ec2d2d2a7a3fb
Jaco <jacogr@gmail.com>
2023-02-19 15:48:14 +0200

    0.44.9

--- jest/setupEnv.js
+++ jest/setupEnv.js
@@ -1,15 +1,16 @@
 // Copyright 2017-2023 @polkadot/extension authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-const nodeCrypto = require('crypto');
-const { TextDecoder } = require('@polkadot/x-textencoder/node');
-const { TextEncoder } = require('@polkadot/x-textencoder/node');
+import nodeCrypto from 'crypto';
 
-window.crypto = {
+import { TextDecoder } from '@polkadot/x-textencoder/node';
+import { TextEncoder } from '@polkadot/x-textencoder/node';
+
+window.crypto ??= {
   getRandomValues: function (buffer) {
     return nodeCrypto.randomFillSync(buffer);
   }
 };
 
-global.TextDecoder = TextDecoder;
-global.TextEncoder = TextEncoder;
+global.TextDecoder ??= TextDecoder;
+global.TextEncoder ??= TextEncoder;

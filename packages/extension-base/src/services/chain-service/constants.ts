58242dc165bf8a1c1e3511fdafe1ad996c781e31
Nam Phạm <namphamc99@gmail.com>
2023-03-30 15:26:59 +0700

    [Issue 1024] fix bug getting Genshiro and Equilibrium balance

--- packages/extension-base/src/services/chain-service/constants.ts
+++ packages/extension-base/src/services/chain-service/constants.ts
@@ -30,7 +30,7 @@ export const _BALANCE_CHAIN_GROUP = {
   genshiro: ['genshiro_testnet', 'genshiro'],
   equilibrium_parachain: ['equilibrium_parachain'],
   bifrost: ['bifrost', 'acala', 'karura', 'acala_testnet', 'pioneer', 'bitcountry'],
-  statemine: ['statemine', 'astar', 'shiden', 'statemint', 'moonbeam', 'moonbase', 'moonriver'],
+  statemine: ['statemine', 'astar', 'shiden', 'statemint', 'moonbeam', 'moonbase', 'moonriver', 'crabParachain'],
   kusama: ['kusama', 'kintsugi', 'kintsugi_test', 'interlay', 'acala', 'statemint', 'karura', 'bifrost'] // perhaps there are some runtime updates
 };
 

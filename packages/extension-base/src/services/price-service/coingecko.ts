a0497bb6c8520580bdb788cf3cf6f0731946ed6f
AnhMTV <maithachvietanh@gmail.com>
2023-04-03 12:06:42 +0700

    Implement backup API for price service

--- packages/extension-base/src/services/price-service/coingecko.ts
+++ packages/extension-base/src/services/price-service/coingecko.ts
@@ -12,16 +12,32 @@ interface GeckoItem {
   symbol: string
 }
 
+let useBackupApi = false;
+
 export const getTokenPrice = async (priceIds: Set<string>, currency = 'usd'): Promise<PriceJson> => {
   try {
     const idStr = Array.from(priceIds).join(',');
-    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&per_page=250&ids=${idStr}`);
+    let res;
+
+    if (!useBackupApi) {
+      try {
+        res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&per_page=250&ids=${idStr}`);
+      } catch (err) {
+        useBackupApi = true;
+      }
+    }
+
+    if (useBackupApi || res?.status !== 200) {
+      console.log('Use backup api for price');
+      useBackupApi = true;
+      res = await axios.get(`https://chain-data.subwallet.app/api/price/get?ids=${idStr}`);
+    }
 
     if (res.status !== 200) {
       console.warn('Failed to get token price');
     }
 
-    const responseData = res.data as Array<GeckoItem>;
+    const responseData = res.data as Array<GeckoItem> || [];
     const priceMap: Record<string, number> = {};
     const price24hMap: Record<string, number> = {};
 

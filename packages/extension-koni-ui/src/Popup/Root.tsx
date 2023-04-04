8e5c596aa92a636ce2d28a252c4eecc742046cb0
S2kael <laiducminh1002@gmail.com>
2023-04-04 20:35:27 +0700

    [Common] Update router

--- packages/extension-koni-ui/src/Popup/Root.tsx
+++ packages/extension-koni-ui/src/Popup/Root.tsx
@@ -32,11 +32,11 @@ const tokenUrl = '/home/tokens';
 const loginUrl = '/keyring/login';
 const createPasswordUrl = '/keyring/create-password';
 const migratePasswordUrl = '/keyring/migrate-password';
-//
-// const baseAccountPath = '/account';
-// const allowImportAccountPaths = ['new-seed-phrase', 'import-seed-phrase', 'import-private-key', 'restore-json', 'import-by-qr', 'attach-read-only', 'connect-parity-signer', 'connect-keystone', 'connect-ledger'];
-//
-// const allowImportAccountUrls = allowImportAccountPaths.map((path) => `${baseAccountPath}/${path}`);
+
+const baseAccountPath = '/account';
+const allowImportAccountPaths = ['new-seed-phrase', 'import-seed-phrase', 'import-private-key', 'restore-json', 'import-by-qr', 'attach-read-only', 'connect-parity-signer', 'connect-keystone', 'connect-ledger'];
+
+const allowImportAccountUrls = allowImportAccountPaths.map((path) => `${baseAccountPath}/${path}`);
 
 function DefaultRoute ({ children }: {children: React.ReactNode}): React.ReactElement {
   const location = useLocation();
@@ -100,28 +100,22 @@ function DefaultRoute ({ children }: {children: React.ReactNode}): React.ReactEl
       navigate(migratePasswordUrl);
     } else if (hasMasterPassword && isLocked) {
       navigate(loginUrl);
-    } else if (pathName === DEFAULT_ROUTER_PATH) {
-      if (hasConfirmations) {
-        if (hasMasterPassword && isLocked) {
-          navigate(loginUrl);
-        } else {
-          openPModal('confirmations');
-        }
-      } else if (isNoAccount(accounts)) {
-        if (hasMasterPassword && isLocked) {
-          navigate(loginUrl);
-        } else {
+    } else if (!hasMasterPassword) {
+      if (isNoAccount(accounts)) {
+        if (![...allowImportAccountUrls, welcomeUrl, createPasswordUrl].includes(pathName)) {
           navigate(welcomeUrl);
         }
-      } else if (!hasMasterPassword) {
+      } else {
         navigate(createPasswordUrl);
-      } else if (isLocked) {
-        navigate(loginUrl);
-      } else if (needMigrate) {
-        navigate(migratePasswordUrl);
+      }
+    } else if (pathName === DEFAULT_ROUTER_PATH) {
+      if (hasConfirmations) {
+        openPModal('confirmations');
       } else {
         navigate(tokenUrl);
       }
+    } else if (pathName === migratePasswordUrl && !needMigrate) {
+      goHome();
     } else if (pathName === loginUrl && !isLocked) {
       goHome();
     } else if (pathName === welcomeUrl && !isNoAccount(accounts)) {

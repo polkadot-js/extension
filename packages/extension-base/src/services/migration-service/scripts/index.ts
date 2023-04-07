32f855e80630cc3084f06844b9e842332572452b
AnhMTV <maithachvietanh@gmail.com>
2023-04-07 18:21:43 +0700

    Migrate Camera Settings

--- packages/extension-base/src/services/migration-service/scripts/index.ts
+++ packages/extension-base/src/services/migration-service/scripts/index.ts
@@ -4,6 +4,7 @@
 import AutoEnableChainsTokens from '@subwallet/extension-base/services/migration-service/scripts/AutoEnableChainsTokens';
 import MigrateImportedToken from '@subwallet/extension-base/services/migration-service/scripts/MigrateImportedToken';
 import MigrateNetworkSettings from '@subwallet/extension-base/services/migration-service/scripts/MigrateNetworkSettings';
+import MigrateSettings from '@subwallet/extension-base/services/migration-service/scripts/MigrateSettings';
 import MigrateTransactionHistory from '@subwallet/extension-base/services/migration-service/scripts/MigrateTransactionHistory';
 
 import BaseMigrationJob from '../Base';
@@ -14,7 +15,7 @@ export default <Record<string, typeof BaseMigrationJob>> {
   '1.0.1-11': MigrateNetworkSettings,
   '1.0.1-20': MigrateImportedToken,
   '1.0.1-30': MigrateTransactionHistory,
-  '1.0.1-40': AutoEnableChainsTokens
-  // '1.0.1-0': ClearOldStorage,
+  '1.0.1-40': AutoEnableChainsTokens,
+  '1.0.1-50': MigrateSettings
   // [`${EVERYTIME}-1`]: AutoEnableChainsTokens
 };

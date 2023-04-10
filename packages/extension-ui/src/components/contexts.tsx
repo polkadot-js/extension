aeeea29b30bda9624f4e1962a89e88b16d4c7766
Jaco <jacogr@gmail.com>
2023-04-10 10:21:41 +0300

    Bump dev (w/ eslint adjustments)

--- packages/extension-ui/src/components/contexts.tsx
+++ packages/extension-ui/src/components/contexts.tsx
@@ -11,24 +11,20 @@ import settings from '@polkadot/ui-settings';
 
 const noop = (): void => undefined;
 
-const AccountContext = React.createContext<AccountsContext>({ accounts: [], hierarchy: [], master: undefined });
-const ActionContext = React.createContext<(to?: string) => void>(noop);
-const AuthorizeReqContext = React.createContext<AuthorizeRequest[]>([]);
-const MediaContext = React.createContext<boolean>(false);
-const MetadataReqContext = React.createContext<MetadataRequest[]>([]);
-const SettingsContext = React.createContext<SettingsStruct>(settings.get());
-const SigningReqContext = React.createContext<SigningRequest[]>([]);
-const ThemeSwitchContext = React.createContext<(theme: AvailableThemes) => void>(noop);
-const ToastContext = React.createContext<({show: (message: string) => void})>({ show: noop });
+export const AccountContext = /*#__PURE__*/ React.createContext<AccountsContext>({ accounts: [], hierarchy: [], master: undefined });
 
-export {
-  AccountContext,
-  ActionContext,
-  AuthorizeReqContext,
-  MediaContext,
-  MetadataReqContext,
-  SettingsContext,
-  SigningReqContext,
-  ThemeSwitchContext,
-  ToastContext
-};
+export const ActionContext = /*#__PURE__*/ React.createContext<(to?: string) => void>(noop);
+
+export const AuthorizeReqContext = /*#__PURE__*/ React.createContext<AuthorizeRequest[]>([]);
+
+export const MediaContext = /*#__PURE__*/ React.createContext<boolean>(false);
+
+export const MetadataReqContext = /*#__PURE__*/ React.createContext<MetadataRequest[]>([]);
+
+export const SettingsContext = /*#__PURE__*/ React.createContext<SettingsStruct>(settings.get());
+
+export const SigningReqContext = /*#__PURE__*/ React.createContext<SigningRequest[]>([]);
+
+export const ToastContext = /*#__PURE__*/ React.createContext<({show: (message: string) => void})>({ show: noop });
+
+export const ThemeSwitchContext = /*#__PURE__*/ React.createContext<(theme: AvailableThemes) => void>(noop);

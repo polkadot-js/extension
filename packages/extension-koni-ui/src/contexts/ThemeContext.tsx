b6319c66c02e36791aac0d15c6628a640952289e
AnhMTV <maithachvietanh@gmail.com>
2023-04-22 19:17:35 +0700

    [Issue-1275] Fix border problems some case like full screen modal (4)

--- packages/extension-koni-ui/src/contexts/ThemeContext.tsx
+++ packages/extension-koni-ui/src/contexts/ThemeContext.tsx
@@ -51,15 +51,13 @@ const GlobalStyle = createGlobalStyle<ThemeProps>(({ theme }) => {
     '.ant-sw-modal .ant-sw-modal-content': {
       width: 390 - token.lineWidth * 2,
       left: token.lineWidth,
-      bottom: token.lineWidth + 1
+      bottom: 0,
+      borderBottom: `1px solid ${token.colorBgInput}`
     },
 
     '.modal-full': {
       '.ant-sw-modal-content': {
-        // boxShadow: `inset 0 0 0 ${token.lineWidth}px ${token.colorBgInput}, inset 0 -${token.lineWidth}px 0 ${token.lineWidth}px ${token.colorBgInput}`,
-
         '.ant-sw-modal-header': {
-          boxShadow: `inset 0 ${token.lineWidth}px 0 ${token.lineWidth}px ${token.colorBgInput}`,
           borderRadius: 0
         }
       }

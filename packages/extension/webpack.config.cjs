e0614846361cf8536d124920c98864820621f41c
Jaco <jacogr@gmail.com>
2023-05-18 12:39:27 +0300

    Adjust tsconfig usage (w/ latest dev)

--- packages/extension/webpack.config.cjs
+++ packages/extension/webpack.config.cjs
@@ -6,5 +6,6 @@ const createConfig = require('./webpack.shared.cjs');
 module.exports = createConfig({
   background: './src/background.ts',
   content: './src/content.ts',
+  extension: './src/extension.ts',
   page: './src/page.ts'
 });

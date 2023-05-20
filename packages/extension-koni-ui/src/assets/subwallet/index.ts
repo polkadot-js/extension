c65354c04a1f3cdf1c34b2d7a8d37ffc2810fa11
AnhMTV <maithachvietanh@gmail.com>
2023-05-20 17:04:56 +0700

    Update subwallet logo for DApp access

--- packages/extension-koni-ui/src/assets/subwallet/index.ts
+++ packages/extension-koni-ui/src/assets/subwallet/index.ts
@@ -5,29 +5,7 @@ import { DefaultLogosMap } from '@subwallet/extension-koni-ui/assets/logo';
 
 const SwLogosMap: Record<string, string> = {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_32: require('./32.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_48: require('./48.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_64: require('./64.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_72: require('./72.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_120: require('./120.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_128: require('./128.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_144: require('./144.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_180: require('./180.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_196: require('./196.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_256: require('./256.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet_512: require('./512.png'),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  subwallet: require('./256.png'),
+  subwallet: require('./subwallet.png'),
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   avatar_placeholder: require('./avatar_placeholder.png'),
   default: DefaultLogosMap.default,

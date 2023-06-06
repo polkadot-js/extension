f8ff3f858bb7ac20b3a708f6c0f2d0c718d38054
Nam Phạm <namphamc99@gmail.com>
2023-06-06 15:44:31 +0700

    [Issue 1352] remove logs

--- packages/extension-base/src/koni/api/xcm/index.ts
+++ packages/extension-base/src/koni/api/xcm/index.ts
@@ -42,7 +42,5 @@ export const createXcmExtrinsic = async ({ chainInfoMap,
     extrinsic = getExtrinsicByXtokensPallet(originTokenInfo, originChainInfo, destinationChainInfo, recipient, sendingValue, api);
   }
 
-  console.log('extrinsic', extrinsic.toHex());
-
   return extrinsic;
 };

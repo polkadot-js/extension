79e0855fa4864aedf48615ebd9a15285dc6725fa
Nam Phạm <namphamc99@gmail.com>
2023-06-06 14:39:31 +0700

    [Issue 1352] update params for XCM

--- packages/extension-base/src/koni/api/xcm/polkadotXcm.ts
+++ packages/extension-base/src/koni/api/xcm/polkadotXcm.ts
@@ -10,14 +10,12 @@ import { ApiPromise } from '@polkadot/api';
 export function getExtrinsicByPolkadotXcmPallet (tokenInfo: _ChainAsset, originChainInfo: _ChainInfo, destinationChainInfo: _ChainInfo, recipientAddress: string, value: string, api: ApiPromise) {
   const weightParam = getDestWeight();
   const beneficiary = getBeneficiary(destinationChainInfo, recipientAddress);
-  const destination = getDestinationChainLocation(destinationChainInfo);
+  const destination = getDestinationChainLocation(originChainInfo, destinationChainInfo);
   const assetLocation = getTokenLocation(tokenInfo, value);
 
   let method = 'limitedReserveTransferAssets';
 
-  if (['astar', 'shiden'].includes(originChainInfo.slug)) {
-    method = 'limitedReserveWithdrawAssets';
-  } else if (_isSubstrateRelayChain(destinationChainInfo)) {
+  if (_isSubstrateRelayChain(destinationChainInfo)) {
     method = 'limitedTeleportAssets';
   }
 

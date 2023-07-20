a184f726304ac51ba13055c3cc70fd298dc50fe7
S2kael <laiducminh1002@gmail.com>
2023-07-20 11:18:34 +0700

    [Issue-1639] Fix auto close

--- packages/extension-koni-ui/src/hooks/common/useNotification.tsx
+++ packages/extension-koni-ui/src/hooks/common/useNotification.tsx
@@ -2,20 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { NotificationContext, NotificationProps } from '@subwallet/react-ui/es/notification/NotificationProvider';
-import React, { useCallback, useContext, useMemo } from 'react';
+import React, { useCallback, useContext, useMemo, useRef } from 'react';
 
 export default function useNotification (): (props: NotificationProps) => void {
   const { showNotification } = useContext(NotificationContext);
+
+  const timeoutRef = useRef<NodeJS.Timer>();
 
   const key = useMemo((): React.Key => {
     return Date.now();
   }, []);
 
   return useCallback((props: NotificationProps) => {
-    showNotification({
-      key,
-      closable: true,
-      ...props
-    });
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      showNotification({
+        key,
+        closable: true,
+        ...props
+      });
+    }, 100);
   }, [key, showNotification]);
 }

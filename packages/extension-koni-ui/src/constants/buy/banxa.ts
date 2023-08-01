2d58f9f7c3980659ba3083bcfa883bbd68f5b2be
S2kael <laiducminh1002@gmail.com>
2023-08-01 10:06:34 +0700

    Fix lint

--- packages/extension-koni-ui/src/constants/buy/banxa.ts
+++ packages/extension-koni-ui/src/constants/buy/banxa.ts
@@ -5,4 +5,4 @@ export const BANXA_SUBDOMAIN = 'subwallet';
 export const BANXA_TEST_MODE = false;
 
 export const BANXA_SANDBOX_API_KEY = BANXA_TEST_MODE ? 'subwallet@test24052023@/*436*/' : 'subwallet@prod202307311219';
-export const BANXA_SANBOX_API_SECRET = BANXA_TEST_MODE? 'JV5TjrKzl9gO8t8GK4oGhvDEnxAVJIc0' : 'UcOdkRLArFwEvQW66eyoxXdFWzoM27pM';
+export const BANXA_SANBOX_API_SECRET = BANXA_TEST_MODE ? 'JV5TjrKzl9gO8t8GK4oGhvDEnxAVJIc0' : 'UcOdkRLArFwEvQW66eyoxXdFWzoM27pM';

26a08cfec9d3f662cfcd1897cb628c74f7d151e5
AnhMTV <maithachvietanh@gmail.com>
2023-08-04 17:17:50 +0700

    Update new logic and their url for webapp

--- packages/extension-koni-ui/src/assets/logo/index.ts
+++ packages/extension-koni-ui/src/assets/logo/index.ts
@@ -4,25 +4,25 @@
 import { _ChainConnectionStatus } from '@subwallet/extension-base/services/chain-service/types';
 
 export const DefaultLogosMap: Record<string, string> = {
-  subwallet: './images/projects/subwallet.png',
-  parity: './images/projects/parity.png',
-  keystone: './images/projects/keystone.png',
-  ledger: './images/projects/ledger.png',
-  default: './images/subwallet/default.png',
-  transak: './images/projects/transak.png',
-  moonpay: './images/projects/moonpay.png',
-  onramper: './images/projects/onramper.png',
-  polkadot_vault: './images/projects/polkadot-vault.png',
-  walletconnect: './images/projects/walletconnect.png',
-  banxa: './images/projects/banxa.png'
+  subwallet: '/images/projects/subwallet.png',
+  parity: '/images/projects/parity.png',
+  keystone: '/images/projects/keystone.png',
+  ledger: '/images/projects/ledger.png',
+  default: '/images/subwallet/default.png',
+  transak: '/images/projects/transak.png',
+  moonpay: '/images/projects/moonpay.png',
+  onramper: '/images/projects/onramper.png',
+  polkadot_vault: '/images/projects/polkadot-vault.png',
+  walletconnect: '/images/projects/walletconnect.png',
+  banxa: '/images/projects/banxa.png'
 };
 
 export const IconMap = {
-  __CONNECTED__: './images/icons/__connected__.png',
-  __CONNECTING__: './images/icons/__connecting__.png',
-  __UNSTABLE__: './images/icons/__unstable__.png',
-  __DISCONNECTED__: './images/icons/__disconnected__.png',
-  __qr_code__: './images/icons/__qr_code__.png'
+  __CONNECTED__: '/images/icons/__connected__.png',
+  __CONNECTING__: '/images/icons/__connecting__.png',
+  __UNSTABLE__: '/images/icons/__unstable__.png',
+  __DISCONNECTED__: '/images/icons/__disconnected__.png',
+  __qr_code__: '/images/icons/__qr_code__.png'
 };
 
 export default DefaultLogosMap;

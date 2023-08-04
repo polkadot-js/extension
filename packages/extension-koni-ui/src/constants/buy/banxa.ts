7a1a9e042f2beba9162a5efd7b39a19c0eefcd84
S2kael <laiducminh1002@gmail.com>
2023-08-04 15:30:56 +0700

    Issue-1746] Update environment

--- packages/extension-koni-ui/src/constants/buy/banxa.ts
+++ packages/extension-koni-ui/src/constants/buy/banxa.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const BANXA_SUBDOMAIN = 'subwallet';
-export const BANXA_TEST_MODE = process.env.BANXA_TEST_MODE ? process.env.BANXA_TEST_MODE === 'true' : true;
+export const BANXA_TEST_MODE = process.env.BANXA_TEST_MODE !== undefined ? !!process.env.BANXA_TEST_MODE : true;
 
 export const BANXA_SANDBOX_API_KEY = process.env.BANXA_SANDBOX_API_KEY || 'subwallet@test24052023@/*436*/';
 export const BANXA_SANBOX_API_SECRET = process.env.BANXA_SANBOX_API_SECRET || 'JV5TjrKzl9gO8t8GK4oGhvDEnxAVJIc0';

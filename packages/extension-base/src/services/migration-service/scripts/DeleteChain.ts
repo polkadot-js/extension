1cfdb50e6fd1ecc4defbf5714aec20e93f533f03
Nam Phạm <namphamc99@gmail.com>
2023-08-11 17:48:23 +0700

    [Issue 1777] fix bug migrating

--- packages/extension-base/src/services/migration-service/scripts/DeleteChain.ts
+++ packages/extension-base/src/services/migration-service/scripts/DeleteChain.ts
@@ -2,14 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import BaseMigrationJob from '@subwallet/extension-base/services/migration-service/Base';
-import Dexie from 'dexie';
 
 export default class DeleteChain extends BaseMigrationJob {
+  // eslint-disable-next-line @typescript-eslint/require-await
   public override async run (): Promise<void> {
-    const db = new Dexie('SubWalletDB_v2');
-    const dexieDB = await db.open();
-    const transactionTable = dexieDB.table('chain');
+    ['snow', 'snow_evm', 'arctic_testnet'].forEach((chain) => {
+      this.state.forceRemoveChain(chain);
+    });
 
-    await transactionTable.bulkDelete(['snow', 'snow_evm', 'arctic_testnet']);
+    console.log('done job');
   }
 }

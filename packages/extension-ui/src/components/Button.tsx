b5c434733083fbf2be56eddf0480434f57934144
Jaco <jacogr@gmail.com>
2023-08-19 23:33:55 +0300

    Adjust

--- packages/extension-ui/src/components/Button.tsx
+++ packages/extension-ui/src/components/Button.tsx
@@ -46,7 +46,7 @@ function Button ({ children, className = '', isBusy, isDisabled, onClick, to }: 
 }
 
 export default styled(Button)<ButtonProps>(({ isDanger }) => `
-  background: ${isDanger ? 'var(--buttonBackgroundDanger)' : 'var(--theme.buttonBackground)'};
+  background: ${isDanger ? 'var(--buttonBackgroundDanger)' : 'var(--buttonBackground)'};
   cursor: pointer;
   display: block;
   width: 100%;

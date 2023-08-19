6bb5f913b8943e0995544f4194980329456f51d7
Jaco <jacogr@gmail.com>
2023-08-19 23:31:11 +0300

    Non-vars overrides

--- packages/extension-ui/src/components/View.tsx
+++ packages/extension-ui/src/components/View.tsx
@@ -1,9 +1,7 @@
 // Copyright 2019-2023 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Theme } from './index.js';
-
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { createGlobalStyle } from 'styled-components';
 
 // FIXME We should not import from index when this one is imported there as well
@@ -15,7 +13,12 @@ interface Props {
 }
 
 function setGlobalTheme (theme: string): void {
-  document?.documentElement?.setAttribute('data-theme', theme);
+  const _theme = theme === 'dark'
+    ? 'dark'
+    : 'light';
+
+  localStorage.setItem('theme', _theme);
+  document?.documentElement?.setAttribute('data-theme', _theme);
 }
 
 function View ({ children, className }: Props): React.ReactElement<Props> {
@@ -23,16 +26,8 @@ function View ({ children, className }: Props): React.ReactElement<Props> {
     setGlobalTheme(chooseTheme());
   }, []);
 
-  const switchTheme = useCallback(
-    (theme: Theme): void => {
-      localStorage.setItem('theme', theme);
-      setGlobalTheme(theme);
-    },
-    []
-  );
-
   return (
-    <ThemeSwitchContext.Provider value={switchTheme}>
+    <ThemeSwitchContext.Provider value={setGlobalTheme}>
       <BodyTheme />
       <Main className={className}>
         {children}

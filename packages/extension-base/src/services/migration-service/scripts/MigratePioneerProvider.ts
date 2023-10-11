5b34b301037347752467b78ab230eac246092a5d
S2kael <laiducminh1002@gmail.com>
2023-10-11 18:35:11 +0700

    [Issue-1998] Refactor MigrateProvider script

--- packages/extension-base/src/services/migration-service/scripts/MigratePioneerProvider.ts
+++ packages/extension-base/src/services/migration-service/scripts/MigratePioneerProvider.ts
@@ -1,28 +1,10 @@
 // Copyright 2019-2022 @subwallet/extension-koni authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import BaseMigrationJob from '@subwallet/extension-base/services/migration-service/Base';
+import MigrateProvider from './MigrateProvider';
 
-export default class MigratePioneerProvider extends BaseMigrationJob {
-  public override async run (): Promise<void> {
-    const state = this.state;
-
-    const slug = 'pioneer';
-    const oldProvider = 'OnFinality';
-    const newProvider = 'Pioneer';
-
-    const chainState = state.getChainStateByKey(slug);
-    const chainInfo = state.getChainInfo(slug);
-
-    if (chainState.active && chainState.currentProvider === oldProvider) {
-      await state.upsertChainInfo({
-        mode: 'update',
-        chainEditInfo: {
-          currentProvider: newProvider,
-          slug: slug,
-          providers: chainInfo.providers
-        }
-      });
-    }
-  }
+export default class MigratePioneerProvider extends MigrateProvider {
+  slug = 'pioneer';
+  oldProvider = 'OnFinality';
+  newProvider = 'Pioneer';
 }

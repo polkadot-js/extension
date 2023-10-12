202fe0dd5d5e6de5b61cfd8e01a15b6b3db76b85
AnhMTV <maithachvietanh@gmail.com>
2023-10-12 11:38:49 +0700

    Hide Earning from webapp

--- packages/extension-koni-ui/src/components/Layout/parts/SideMenu/SideMenu.tsx
+++ packages/extension-koni-ui/src/components/Layout/parts/SideMenu/SideMenu.tsx
@@ -9,7 +9,7 @@ import { ThemeProps } from '@subwallet/extension-koni-ui/types';
 import { openInNewTab } from '@subwallet/extension-koni-ui/utils';
 import { Button, Icon, Image } from '@subwallet/react-ui';
 import CN from 'classnames';
-import { ArrowCircleLeft, ArrowCircleRight, ArrowSquareUpRight, Clock, Gear, Globe, Info, MessengerLogo, Rocket, Vault, Wallet } from 'phosphor-react';
+import { ArrowCircleLeft, ArrowCircleRight, ArrowSquareUpRight, Clock, Database, Gear, Globe, Info, MessengerLogo, Rocket, Wallet } from 'phosphor-react';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -47,16 +47,16 @@ function Component ({ className,
         value: '/home/crowdloans',
         icon: Rocket
       },
-      // {
-      //   label: t('Staking'),
-      //   value: '/home/staking',
-      //   icon: Database
-      // },
       {
-        label: t('Earning'),
-        value: '/home/earning/',
-        icon: Vault
+        label: t('Staking'),
+        value: '/home/staking',
+        icon: Database
       },
+      // {
+      //   label: t('Earning'),
+      //   value: '/home/earning/',
+      //   icon: Vault
+      // },
       {
         label: t('DApps'),
         value: '/dapps',

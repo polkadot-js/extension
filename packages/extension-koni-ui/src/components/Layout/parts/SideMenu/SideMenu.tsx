48275520e79066c9bcd679909af10c158ccb4a18
AnhMTV <maithachvietanh@gmail.com>
2023-10-16 10:23:31 +0700

    Re-enable mission pool

--- packages/extension-koni-ui/src/components/Layout/parts/SideMenu/SideMenu.tsx
+++ packages/extension-koni-ui/src/components/Layout/parts/SideMenu/SideMenu.tsx
@@ -9,7 +9,7 @@ import { ThemeProps } from '@subwallet/extension-koni-ui/types';
 import { openInNewTab } from '@subwallet/extension-koni-ui/utils';
 import { Button, Icon, Image } from '@subwallet/react-ui';
 import CN from 'classnames';
-import { ArrowCircleLeft, ArrowCircleRight, ArrowSquareUpRight, Clock, Gear, Globe, Info, MessengerLogo, Rocket, Vault, Wallet } from 'phosphor-react';
+import { ArrowCircleLeft, ArrowCircleRight, ArrowSquareUpRight, Clock, Gear, Globe, Info, MessengerLogo, Parachute, Rocket, Vault, Wallet } from 'phosphor-react';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 

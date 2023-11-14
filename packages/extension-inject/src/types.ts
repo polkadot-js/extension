bc965313ef0039ddaf9fb52ae8236b064eed18ee
S2kael <laiducminh1002@gmail.com>
2023-11-14 10:06:53 +0700

    [Issue-2021] Init data

--- packages/extension-inject/src/types.ts
+++ packages/extension-inject/src/types.ts
@@ -124,3 +124,24 @@ export interface Web3AccountsOptions {
   ss58Format?: number,
   accountType?: KeypairType[]
 }
+
+export interface EIP6963ProviderInfo {
+  uuid: string;
+  name: string;
+  icon: string;
+  rdns: string;
+}
+
+export interface EIP6963ProviderDetail {
+  info: EIP6963ProviderInfo;
+  provider: EvmProvider;
+}
+
+export interface EIP6963AnnounceProviderEvent extends CustomEvent {
+  type: "eip6963:announceProvider";
+  detail: EIP6963ProviderDetail;
+}
+
+export interface EIP6963RequestProviderEvent extends Event {
+  type: "eip6963:requestProvider";
+}

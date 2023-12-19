52b167ca53664dbfab9a9f91bb67d6c931c006a7
AnhMTV <maithachvietanh@gmail.com>
2023-12-19 18:31:54 +0700

    [Issue-2381] Add api reload balance, and crowdloan

--- packages/extension-base/src/koni/background/handlers/Extension.ts
+++ packages/extension-base/src/koni/background/handlers/Extension.ts
@@ -3460,6 +3460,10 @@ export default class KoniExtension {
       return await this.#koniState.reloadNft();
     } else if (data === 'staking') {
       return await this.#koniState.reloadStaking();
+    } else if (data === 'balance') {
+      return await this.#koniState.reloadBalance();
+    } else if (data === 'crowdloan') {
+      return await this.#koniState.reloadCrowdloan();
     }
 
     return Promise.resolve(false);

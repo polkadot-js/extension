7db0e31717c76d8a776f0804d362d0ba6f92096a
Jaco <jacogr@gmail.com>
2023-12-19 10:48:25 +0200

    Bump deps

--- packages/extension-dapp/src/detectOther.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2017-2023 @polkadot/extension-dapp authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import { packageInfo as injectInfo } from '@polkadot/extension-inject/packageInfo';
-
-export default [injectInfo];

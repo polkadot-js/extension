7dfd3395a24538a3129e8fb06c4eb7843dfcf4fe
Jaco <jacogr@gmail.com>
2024-01-08 14:59:28 +0200

    2024

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,4 @@
-// Copyright 2019-2023 @polkadot/extension authors & contributors
+// Copyright 2019-2024 @polkadot/extension authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = require('@polkadot/dev/config/prettier.cjs');

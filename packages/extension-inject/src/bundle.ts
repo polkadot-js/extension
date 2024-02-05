9c5ee820998274614d208ec1edb11ab2cffdae44
AnhMTV <maithachvietanh@gmail.com>
2024-02-05 11:46:10 +0700

    [Issue-2588] Fix bug can not open Uniswap

--- packages/extension-inject/src/bundle.ts
+++ packages/extension-inject/src/bundle.ts
@@ -66,14 +66,19 @@ export const inject6963EIP = (provider: EvmProvider) => {
   };
 
   const _provider = new Proxy(provider, {
-    // TODO: Please review the security of this
     get (target, key) {
       if (key === 'then') {
         return Promise.resolve(target);
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
-      return Reflect.get(target, key).bind(target);
+      const proxyTarget = Reflect.get(target, key) as Record<string, any>;
+
+      if (typeof proxyTarget?.bind === 'function') {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
+        return proxyTarget.bind(target);
+      }
+
+      return proxyTarget;
     },
     deleteProperty () {
       return true;

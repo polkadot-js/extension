b48ccdc4fe3725e97aa0a6e4b3332445c33b634a
S2kael <laiducminh1002@gmail.com>
2024-02-19 12:16:06 +0700

    [Issue-2617] Add chain asset test script

--- packages/extension-base/src/services/chain-service/health-check/provider.spec.ts
+++ packages/extension-base/src/services/chain-service/health-check/provider.spec.ts
@@ -1,117 +1,12 @@
 // Copyright 2019-2022 @subwallet/extension-base authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { cryptoWaitReady } from '@polkadot/util-crypto';
 import { ChainInfoMap } from '@subwallet/chain-list';
 import { _ChainStatus } from '@subwallet/chain-list/types';
-import { EvmApi } from '@subwallet/extension-base/services/chain-service/handler/EvmApi';
-import { _EvmApi, _SubstrateApi } from '@subwallet/extension-base/services/chain-service/types';
-
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { evmHandleConnectChain, substrateHandleConnectChain, timeoutMessage } from './base';
 
 jest.setTimeout(3 * 60 * 60 * 1000);
-
-const failedMessage = 'Connect failed';
-const timeoutMessage = 'Connect timeout';
-
-const substrateHandleConnectChain = async (chain: string, key: string, provider: string, hash: string): Promise<[_SubstrateApi | null, string]> => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises,no-async-promise-executor
-  return new Promise<[_SubstrateApi | null, string]>(async (resolve) => {
-    console.log('start', chain, key, provider);
-
-    const api: _SubstrateApi | null = null;
-
-    const _api = new ApiPromise({ provider: new WsProvider(provider) });
-
-    let logFail = true;
-
-    const handlerOnFail = (e: Error) => {
-      if (logFail) {
-        console.log('error', chain, key);
-        resolve([api, e?.message || failedMessage]);
-      }
-
-      logFail = false;
-    };
-
-    const timeout = setTimeout(() => {
-      console.log('timeout', chain, key);
-      resolve([api, timeoutMessage]);
-      logFail = false;
-
-      _api.disconnect().catch(console.error);
-      _api.off('disconnected', handlerOnFail);
-      _api.off('error', handlerOnFail);
-    }, 30 * 1000);
-
-    _api.on('disconnected', handlerOnFail);
-    _api.on('error', handlerOnFail);
-
-    const temp = await _api.isReady;
-
-    logFail = false;
-
-    _api.off('disconnected', handlerOnFail);
-    _api.off('error', handlerOnFail);
-    clearTimeout(timeout);
-
-    const tempHash = temp.genesisHash.toHex();
-
-    if (hash !== tempHash) {
-      resolve([api, 'Wrong genesisHash']);
-    }
-
-    resolve([api, '']);
-  });
-};
-
-const evmHandleConnectChain = async (chain: string, key: string, provider: string, chainId: number): Promise<[_EvmApi | null, string]> => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises,no-async-promise-executor
-  return new Promise<[_EvmApi | null, string]>(async (resolve) => {
-    console.log('start', chain, key, provider);
-
-    let api: _EvmApi | null = null;
-
-    const _api = new EvmApi(chain, provider, { providerName: key });
-
-    let logFail = true;
-
-    const handlerOnFail = (e: Error) => {
-      if (logFail) {
-        console.log('error', chain, key);
-        resolve([api, e?.message || failedMessage]);
-      }
-
-      logFail = false;
-    };
-
-    const timeout = setTimeout(() => {
-      console.log('timeout', chain, key);
-      resolve([api, timeoutMessage]);
-      logFail = false;
-
-      _api.destroy().catch(console.error);
-    }, 60 * 1000);
-
-    try {
-      api = await _api.isReady;
-    } catch (e) {
-      handlerOnFail(e as Error);
-    }
-
-    logFail = false;
-
-    clearTimeout(timeout);
-
-    const tempId = await _api.api.eth.getChainId();
-
-    if (tempId !== chainId) {
-      resolve([api, 'Wrong chain id']);
-    }
-
-    resolve([api, '']);
-  });
-};
 
 describe('test chain provider', () => {
   beforeAll(async () => {
@@ -161,7 +56,7 @@ describe('test chain provider', () => {
                   errorProvider[chain] = errorProvider[chain] ? [...errorProvider[chain], value] : [value];
                 }
 
-                await api?.destroy();
+                await api?.disconnect();
 
                 clearTimeout(timeout);
               } else {

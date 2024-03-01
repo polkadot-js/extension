2df85cbbfa9a0186d674f39d48fd2d3b9924ed75
nampc <namphamc99@gmail.com>
2024-03-01 18:56:59 +0700

    [Issue 2535] update dependency for swap

--- packages/extension-web-ui/src/messaging/transaction/swap.ts
+++ packages/extension-web-ui/src/messaging/transaction/swap.ts
@@ -1,7 +1,7 @@
 // Copyright 2019-2022 @subwallet/extension-web-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { SwapRequest, SwapSubmitParams } from '@subwallet/extension-base/types/swap';
+import {SwapRequest, SwapSubmitParams, ValidateSwapProcessParams} from '@subwallet/extension-base/types/swap';
 import { sendMessage } from '@subwallet/extension-web-ui/messaging';
 
 export async function handleSwapRequest (request: SwapRequest) {
@@ -14,4 +14,8 @@ export async function getLatestSwapQuote (request: SwapRequest) {
 
 export async function handleSwapStep (request: SwapSubmitParams) {
   return sendMessage('pri(swapService.handleSwapStep)', request);
+}
+
+export async function validateSwapProcess (request: ValidateSwapProcessParams) {
+  return sendMessage('pri(swapService.validateSwapProcess)', request);
 }

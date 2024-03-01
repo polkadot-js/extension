b70757b45bd4c0b513c3dff0e4ea006f82491b84
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-03-01 11:46:35 +0700

    [Issue-2658] disable ternoa_alphanet staking

--- packages/extension-base/src/services/earning-service/constants/chains.ts
+++ packages/extension-base/src/services/earning-service/constants/chains.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const _STAKING_CHAIN_GROUP = {
-  relay: ['polkadot', 'kusama', 'aleph', 'polkadex', 'ternoa', 'alephTest', 'polkadexTest', 'westend', 'kate', 'edgeware', 'creditcoin', 'vara_network', 'goldberg_testnet'],
+  relay: ['polkadot', 'kusama', 'aleph', 'polkadex', 'ternoa',  'alephTest', 'polkadexTest', 'westend', 'kate', 'edgeware', 'creditcoin', 'vara_network', 'goldberg_testnet'],
   para: ['moonbeam', 'moonriver', 'moonbase', 'turing', 'turingStaging', 'bifrost', 'bifrost_testnet', 'calamari_test', 'calamari', 'manta_network'],
   astar: ['astar', 'shiden', 'shibuya'],
   amplitude: ['amplitude', 'amplitude_test', 'kilt', 'kilt_peregrine', 'pendulum', 'krest_network'], // amplitude and kilt only share some common logic
@@ -10,7 +10,7 @@ export const _STAKING_CHAIN_GROUP = {
   nominationPool: ['polkadot', 'kusama', 'westend', 'alephTest', 'aleph', 'kate', 'vara_network', 'goldberg_testnet'],
   bifrost: ['bifrost', 'bifrost_testnet'],
   aleph: ['aleph', 'alephTest'], // A0 has distinct tokenomics
-  ternoa: ['ternoa', 'ternoa_alphanet'],
+  ternoa: ['ternoa'],
   liquidStaking: ['bifrost_dot', 'acala', 'parallel', 'moonbeam'],
   lending: ['interlay'],
   krest_network: ['krest_network']

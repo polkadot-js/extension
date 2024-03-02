cff1f99e2e64e5b8e9ab9beee9d663cfcdcf5ee9
nampc <namphamc99@gmail.com>
2024-03-02 17:04:27 +0700

    [Issue 2535] fix eslint

--- packages/extension-base/src/services/transaction-service/utils.ts
+++ packages/extension-base/src/services/transaction-service/utils.ts
@@ -3,16 +3,9 @@
 
 import { _ChainInfo } from '@subwallet/chain-list/types';
 import { ExtrinsicDataTypeMap, ExtrinsicType } from '@subwallet/extension-base/background/KoniTypes';
-import {
-  _getBlockExplorerFromChain,
-  _isChainTestNet,
-  _isPureEvmChain
-} from '@subwallet/extension-base/services/chain-service/utils';
-import {SwapTxData} from "@subwallet/extension-base/types/swap";
-import {
-  CHAIN_FLIP_MAINNET_EXPLORER,
-  CHAIN_FLIP_TESTNET_EXPLORER
-} from "@subwallet/extension-base/services/swap-service/utils";
+import { _getBlockExplorerFromChain, _isChainTestNet, _isPureEvmChain } from '@subwallet/extension-base/services/chain-service/utils';
+import { CHAIN_FLIP_MAINNET_EXPLORER, CHAIN_FLIP_TESTNET_EXPLORER } from '@subwallet/extension-base/services/swap-service/utils';
+import { SwapTxData } from '@subwallet/extension-base/types/swap';
 
 // @ts-ignore
 export function parseTransactionData<T extends ExtrinsicType> (data: unknown): ExtrinsicDataTypeMap[T] {

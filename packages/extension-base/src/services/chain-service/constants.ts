71ec9bd6f441b01e53ecda4f6b17a1b77d91de51
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-03-12 21:01:32 +0700

    [Issue-2694] Update ChainAsset, support zkAstar, CreditCoinEVM, update explorer for continuum

--- packages/extension-base/src/services/chain-service/constants.ts
+++ packages/extension-base/src/services/chain-service/constants.ts
@@ -241,7 +241,8 @@ export const _DEFAULT_ACTIVE_CHAINS = [
 
 export const EVM_PASS_CONNECT_STATUS: Record<string, string[]> = {
   arbitrum_one: ['*'],
-  okxTest: ['*']
+  okxTest: ['*'],
+  astarZkEvm: ['*']
 };
 
 export const EVM_REFORMAT_DECIMALS = {

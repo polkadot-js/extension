4442f568accc478bd61922f0ae0e9b1785c6e1f6
nguyentiendung <dungtpa2@gmail.com>
2024-03-12 18:22:10 +0700

    Improve UI for swap

--- packages/extension-web-ui/src/Popup/Confirmations/variants/Transaction/variants/Swap.tsx
+++ packages/extension-web-ui/src/Popup/Confirmations/variants/Transaction/variants/Swap.tsx
@@ -152,7 +152,7 @@ const SwapTransactionConfirmation = styled(Component)<Props>(({ theme: { token }
       marginTop: 10
     },
     '.__swap-arrival-time': {
-      marginBottom: 12
+      marginTop: 12
     },
     '.__summary-quote': {
       display: 'flex',
@@ -165,6 +165,9 @@ const SwapTransactionConfirmation = styled(Component)<Props>(({ theme: { token }
       paddingBottom: 16,
       borderRadius: 8,
       marginBottom: 20
+    },
+    '.__swap-quote-expired': {
+      marginTop: 12
     },
     '.__summary-to, .__summary-from': {
       display: 'flex',

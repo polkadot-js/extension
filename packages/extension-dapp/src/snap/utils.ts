0a56ef4bf03ce2c47012ffe920fd4849d70652af
Kami <ekbatanifard@gmail.com>
2024-03-14 16:07:59 +0330

    Update utils.ts

--- packages/extension-dapp/src/snap/utils.ts
+++ packages/extension-dapp/src/snap/utils.ts
@@ -1,21 +1,4 @@
-// Copyright 2019-2023 @polkadot/extension-dapp authors & contributors
+// Copyright 2019-2024 @polkadot/extension-dapp authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 export const hasMetamask = window.ethereum?.isMetaMask;
-
-export async function getLatestPackageVersion(): Promise<string> {
-  try {
-    const response = await fetch(`https://raw.githubusercontent.com/PolkaGate/snap/main/packages/snap/snap.manifest.json`);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch package information. Status: ${response.status}`);
-    }
-
-    const responseData = await response.json();
-    const latestVersion = responseData.version;
-
-    return latestVersion;
-  } catch (error: any) {
-    console.error('Error fetching package information:', error.message);
-    throw error;
-  }
-}

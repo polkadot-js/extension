b1a0f6aca1513acb9224a80239d69e4a8302b5af
nampc <namphamc99@gmail.com>
2024-03-22 15:22:43 +0700

    [Issue 2786] feat: enable some tokens automatically

--- packages/extension-base/src/services/migration-service/scripts/index.ts
+++ packages/extension-base/src/services/migration-service/scripts/index.ts
@@ -53,7 +53,7 @@ export default <Record<string, typeof BaseMigrationJob>>{
   '1.1.41-01': DeleteChainStaking,
   '1.1.44-01': MigrateAssetSetting,
   '1.1.45-01': MigrateTransactionHistoryBySymbol,
-  [`${EVERYTIME}-1.1.46-01`]: AutoEnableSomeTokens
+  '1.1.46-01': AutoEnableSomeTokens
   // [`${EVERYTIME}-1.1.42-02`]: MigrateTransactionHistoryBySymbol
   // [`${EVERYTIME}-1`]: AutoEnableChainsTokens
 };

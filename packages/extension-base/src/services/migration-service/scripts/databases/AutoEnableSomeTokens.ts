ebf7d2bf643ae5ff5bfc4e80bd59a7df5c1a4c69
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-03-22 14:43:25 +0700

    [Issue-2786] Auto enable some tokens AssetHub: DOT, DED, PINK; Moonbeam: xcDOT, xcPINK

---
+++ packages/extension-base/src/services/migration-service/scripts/databases/AutoEnableSomeTokens.ts
@@ -0,0 +1,30 @@
+// Copyright 2019-2022 @subwallet/extension-koni authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { AssetSetting } from '@subwallet/extension-base/background/KoniTypes';
+import BaseMigrationJob from '@subwallet/extension-base/services/migration-service/Base';
+
+export default class AutoEnableSomeTokens extends BaseMigrationJob {
+  public override async run (): Promise<void> {
+    try {
+      const slugs = ['statemint-NATIVE-DOT', 'statemint-LOCAL-DED', 'statemint-LOCAL-PINK', 'moonbeam-LOCAL-xcDOT', 'moonbeam-LOCAL-xcPINK']
+
+      const assetSetting = await this.state.chainService.getAssetSettings();
+
+      const migratedAssetSetting: Record<string, AssetSetting> = {};
+
+      for (const slug of slugs) {
+        if (Object.keys(assetSetting).includes(slug)) {
+          migratedAssetSetting[slug] = { visible: true };
+        }
+      }
+
+      this.state.chainService.setAssetSettings({
+        ...assetSetting,
+        ...migratedAssetSetting
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}

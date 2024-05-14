90664bcf9f0fbee81ed11abf878278f8181240bf
S2kael <laiducminh1002@gmail.com>
2024-05-14 12:20:40 +0700

    [Issue-3056] Ignore for test

--- packages/extension-web-ui/src/constants/enviroment.ts
+++ packages/extension-web-ui/src/constants/enviroment.ts
@@ -1,7 +1,7 @@
 // Copyright 2019-2022 @subwallet/extension-web-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-const PRODUCTION_BRANCHES = ['master', 'webapp', 'webapp-dev'];
+const PRODUCTION_BRANCHES = ['master', 'webapp', 'webapp-dev', 'subwallet-dev'];
 const branchName = process.env.BRANCH_NAME || 'subwallet-dev';
 
 export const isProductionMode = PRODUCTION_BRANCHES.indexOf(branchName) > -1;

805662291af04964293f0fda730cdcd201b0d90e
nguyentiendung <dungtpa2@gmail.com>
2024-05-23 14:29:35 +0700

    [Issue-3093] WebApp - Support GRC-20 token

--- packages/extension-web-ui/src/hooks/screen/nft/index.ts
+++ packages/extension-web-ui/src/hooks/screen/nft/index.ts
@@ -2,5 +2,5 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export { default as useGetFungibleContractSupportedChains } from './useGetFungibleContractSupportedChains';
-export { default as useGetNftContractSupportedChains } from './useGetFungibleContractSupportedChains';
+export { default as useGetNftContractSupportedChains } from './useGetNftContractSupportedChains';
 export { default as useGetNftByAccount } from './useGetNftByAccount';

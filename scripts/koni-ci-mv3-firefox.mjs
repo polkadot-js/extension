69583754c91e3cca3350f52f59a015d92b83e6f2
Thiendekaco <thienguo@gmail.com>
2024-06-20 12:14:31 +0700

    [Issue 3133] [update] Extension - merge newest code of subwallet-dev branch

--- scripts/koni-ci-mv3-firefox.mjs
+++ scripts/koni-ci-mv3-firefox.mjs
@@ -24,7 +24,7 @@ async function updateManifest() {
     if (manifest.web_accessible_resources?.[0]) {
       delete manifest.web_accessible_resources[0].use_dynamic_url;
     }
-
+    manifest.permissions.push("activeTab");
     manifest = {...manifest, host_permissions : ["<all_urls>"], optional_permissions : ["activeTab"]};
 
     if(manifest.content_scripts && manifest.content_scripts.length > 0) {

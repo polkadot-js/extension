4bf036e6c99429fc207efe51b87eff9d11650f1e
nampc <namphamc99@gmail.com>
2024-06-22 14:21:23 +0700

    [Issue 1984] fix: add send fee token for snowbridge

--- packages/extension-base/src/services/balance-service/transfer/xcm/snowBridge.ts
+++ packages/extension-base/src/services/balance-service/transfer/xcm/snowBridge.ts
@@ -2,15 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { _ChainAsset, _ChainInfo } from '@subwallet/chain-list/types';
+import { _Address } from '@subwallet/extension-base/background/KoniTypes';
 import { getWeb3Contract } from '@subwallet/extension-base/koni/api/contract-handler/evm/web3';
 import { _SNOWBRIDGE_GATEWAY_ABI, getSnowBridgeGatewayContract } from '@subwallet/extension-base/koni/api/contract-handler/utils';
 import { _EvmApi } from '@subwallet/extension-base/services/chain-service/types';
 import { _getContractAddressOfToken, _getSubstrateParaId, _isChainEvmCompatible } from '@subwallet/extension-base/services/chain-service/utils';
 import { calculateGasFeeParams } from '@subwallet/extension-base/services/fee-service/utils';
 import { TransactionConfig } from 'web3-core';
+import { Contract } from 'web3-eth-contract';
 
 import { u8aToHex } from '@polkadot/util';
 import { decodeAddress } from '@polkadot/util-crypto';
+
+async function getSendFeeToken (contract: Contract, tokenContract: _Address, destChainParaId: number, destinationFee: string): Promise<string> {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
+  const quoteSendTokenFee = contract.methods.quoteSendTokenFee(tokenContract, destChainParaId, destinationFee);
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+  return (await quoteSendTokenFee.call()) as string;
+}
 
 export async function getSnowBridgeEvmTransfer (tokenInfo: _ChainAsset, originChainInfo: _ChainInfo, destinationChainInfo: _ChainInfo, sender: string, recipientAddress: string, value: string, evmApi: _EvmApi): Promise<TransactionConfig> {
   const snowBridgeContractAddress = getSnowBridgeGatewayContract(originChainInfo.slug);
@@ -27,7 +37,11 @@ export async function getSnowBridgeEvmTransfer (tokenInfo: _ChainAsset, originCh
   const transferCall = snowBridgeContract.methods.sendToken(tokenContract, destinationChainParaId, recipient, destinationFee, value);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
   const transferEncodedCall = transferCall.encodeABI() as string;
-  const priority = await calculateGasFeeParams(evmApi, evmApi.chainSlug);
+
+  const [priority, sendTokenFee] = await Promise.all([
+    calculateGasFeeParams(evmApi, evmApi.chainSlug),
+    getSendFeeToken(snowBridgeContract, tokenContract, destinationChainParaId, destinationFee)
+  ]);
 
   let gasLimit;
 
@@ -41,6 +55,7 @@ export async function getSnowBridgeEvmTransfer (tokenInfo: _ChainAsset, originCh
   return {
     from: sender,
     to: snowBridgeContractAddress,
+    value: sendTokenFee,
     data: transferEncodedCall,
     gas: gasLimit,
     gasPrice: priority.gasPrice,

abeb4e5c0ce90b513e7d40f1ecfb048f7935e249
bee344 <alberto.penayo@parity.io>
2024-07-02 10:30:55 -0300

    fix  in mock

--- packages/extension-mocks/src/chrome.ts
+++ packages/extension-mocks/src/chrome.ts
@@ -28,17 +28,26 @@ const messagingFake = new MessagingFake();
 
 chrome.runtime.connect.returns(messagingFake);
 
-chrome.storage.local.get.returns({
-  authUrls: JSON.stringify({
-    'localhost:3000': {
-      authorizedAccounts: ['5FbSap4BsWfjyRhCchoVdZHkDnmDm3NEgLZ25mesq4aw2WvX'],
-      count: 0,
-      id: '11',
-      origin: 'example.com',
-      url: 'http://localhost:3000'
+chrome.storage.local.get.returns(
+  new Promise<Record<string, any>>((resolve, reject) => {
+    try {
+      const result = {
+        authUrls: JSON.stringify({
+          'localhost:3000': {
+            authorizedAccounts: ['5FbSap4BsWfjyRhCchoVdZHkDnmDm3NEgLZ25mesq4aw2WvX'],
+            count: 0,
+            id: '11',
+            origin: 'example.com',
+            url: 'http://localhost:3000'
+          }
+        })
+      };
+
+      resolve(result);
+    } catch (error) {
+      reject(error);
     }
-  })
-});
+  }));
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access
 (window as any).chrome = (globalThis as any).chrome = chrome;

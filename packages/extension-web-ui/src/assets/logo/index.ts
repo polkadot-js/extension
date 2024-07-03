e88b7b635fcf0d81146904eedfc73ab36340bc32
nguyentiendung <dungtpa2@gmail.com>
2024-07-03 12:19:18 +0700

    [Issue-3256] Fix not show logo

--- packages/extension-web-ui/src/assets/logo/index.ts
+++ packages/extension-web-ui/src/assets/logo/index.ts
@@ -47,7 +47,7 @@ export const DefaultLogosMap: Record<string, string> = {
   currency_cny: '/images/projects/CurrencyCNY.png',
   currency_hkd: '/images/projects/CurrencyHKD.png',
   currency_vnd: '/images/projects/CurrencyVND.png',
-  [SUBSTRATE_GENERIC_KEY]: './images/projects/polkadot.png'
+  [SUBSTRATE_GENERIC_KEY]: '/images/projects/polkadot.png'
 };
 
 export const IconMap = {

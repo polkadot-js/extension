1fab0828a7ab816e4e3bee5ab880f24878a33e70
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2024-07-31 10:16:24 -0700

    Add CI script to check for diffs in src vs build for store release (#1429)
    * Add CI script for diffs
    
    * Cleanup
    
    * Update github actions
    
    * simplify
    
    * echo
    
    * cleanup
    
    * fix
    
    * "$RUNNER_OS" == "Linux"
    
    * hack

---
+++ scripts/diff.sh
@@ -0,0 +1,53 @@
+# Copyright 2019-2024 @polkadot/extension-compat-metamask authors & contributors
+# SPDX-License-Identifier: Apache-2.0
+
+#!/bin/bash
+
+# build firefox target
+yarn build:ff
+
+# Reorg the builds to 
+mkdir ff-diff
+
+OS=$(uname)
+FILE_PATH="./ff-diff"
+
+compare_directories() {
+    local dir1=$1
+    local dir2=$2
+
+    if diff -qr "$dir1" "$dir2" | sort; then
+        echo "Builds are identical"
+        exit 0
+    else
+        echo "Builds are not identical"
+        exit 1
+    fi
+}
+
+unzip_ff() {
+
+    unzip -o master-ff-src.zip -d master-ff-src
+    unzip -o master-ff-build.zip -d master-ff-build
+
+    cd ./master-ff-src && yarn install && yarn build:ff && cd ..
+}
+
+if [ "$OS" == "Darwin" || "$RUNNER_OS" == "Linux" ]; then
+    echo "Running on macOS"
+    # macOS-specific commands go here
+
+    mv ./master-ff-src.zip ./master-ff-build.zip $FILE_PATH && cd $FILE_PATH
+
+    unzip_ff
+    
+    compare_directories ./master-ff-build ./master-ff-src/packages/extension/build
+else # Assuming it will be Linux
+    echo "Running on Linux"
+    # Linux-specific commands go here
+
+    mv -t $FILE_PATH ./master-ff-src.zip ./master-ff-build.zip && cd $FILE_PATH
+    unzip_ff
+    
+    compare_directories ./master-ff-build ./master-ff-src/packages/extension/build
+fi

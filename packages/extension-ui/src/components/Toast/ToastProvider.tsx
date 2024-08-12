3aa32bad208b12434edb9d47ef12c3a21fb8ebb6
F-OBrien <francis@polymesh.network>
2024-08-12 15:10:24 +0100

    fix: revert ToastProvider to use setTimeout

--- packages/extension-ui/src/components/Toast/ToastProvider.tsx
+++ packages/extension-ui/src/components/Toast/ToastProvider.tsx
@@ -1,9 +1,7 @@
 // Copyright 2019-2024 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/* global chrome */
-
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { ToastContext } from '../index.js';
 import Toast from './Toast.js';
@@ -13,37 +11,18 @@ interface ToastProviderProps {
 }
 
 const TOAST_TIMEOUT = 1500;
-const ALARM_NAME = 'toastAlarm';
 
 const ToastProvider = ({ children }: ToastProviderProps): React.ReactElement<ToastProviderProps> => {
   const [content, setContent] = useState('');
   const [visible, setVisible] = useState(false);
 
-  useEffect(() => {
-    const createAlarm = async () => {
-      try {
-        await chrome.alarms.create(ALARM_NAME, { delayInMinutes: TOAST_TIMEOUT / 60000 });
-        chrome.alarms.onAlarm.addListener((alarm) => {
-          if (alarm.name === ALARM_NAME) {
-            setVisible(false);
-          }
-        });
-      } catch (error) {
-        console.error('Error creating the alarm:', error);
-        throw error;
-      }
-    };
+  const show = useCallback((message: string): () => void => {
+    const timerId = setTimeout(() => setVisible(false), TOAST_TIMEOUT);
 
-    createAlarm().then(async () => {
-      return await chrome.alarms.clear(ALARM_NAME);
-    }).catch((error) => console.error('Error clearing the alarm: ', error));
-  }, []);
-
-  const show = useCallback((message: string): (() => void) => {
     setContent(message);
     setVisible(true);
 
-    return (): void => setVisible(false);
+    return (): void => clearTimeout(timerId);
   }, []);
 
   return (

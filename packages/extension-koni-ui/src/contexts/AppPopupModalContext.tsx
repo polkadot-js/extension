27a314724999bc7f83365c368adfda4cdd5e19f2
Frenkie Nguyen <nguyentiendung.me@gmail.com>
2024-08-27 18:55:51 +0700

    [Issue-3477] fix condition show popup

--- packages/extension-koni-ui/src/contexts/AppPopupModalContext.tsx
+++ packages/extension-koni-ui/src/contexts/AppPopupModalContext.tsx
@@ -7,7 +7,6 @@ import { RootState } from '@subwallet/extension-koni-ui/stores';
 import { ModalContext } from '@subwallet/react-ui';
 import React, { useCallback, useContext, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useLocalStorage } from 'usehooks-ts';
 
 import AppPopupModal from '../components/Modal/Campaign/AppPopupModal';
 import { AppContentButton, PopupFrequency } from '../types/staticContent';
@@ -37,14 +36,11 @@ export const AppPopupModalContextProvider = ({ children }: AppPopupModalContextP
   const [appPopupModal, setAppPopupModal] = useState<AppPopupModalInfo>({});
   const { activeModal, inactiveModal } = useContext(ModalContext);
   const { isPopupVisible } = useSelector((state: RootState) => state.campaign);
-  const [storageEarningPosition] = useLocalStorage<string| undefined>(EARNING_WARNING_ANNOUNCEMENT, undefined);
+  const storageEarningPosition = window.localStorage.getItem(EARNING_WARNING_ANNOUNCEMENT);
 
   // TODO: This is a hotfix solution; a better solution must be found.
   const openAppPopupModal = useCallback((data: AppPopupModalInfo) => {
-    const shouldShowPopup =
-      isPopupVisible && (!storageEarningPosition || storageEarningPosition.includes('confirmed'));
-
-    if (shouldShowPopup) {
+    if (isPopupVisible && ((!storageEarningPosition || storageEarningPosition.includes('confirmed')))) {
       setAppPopupModal(data);
       activeModal(APP_POPUP_MODAL);
     }

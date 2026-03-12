a1c87fa383b0a34eb93965cbc7efefc1573e5bc8
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2026-03-12 13:27:39 -0400

    Set headers to 2026 (#1609)

--- packages/extension-dapp/src/bundle.ts
+++ packages/extension-dapp/src/bundle.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2025 @polkadot/extension-dapp authors & contributors
+// Copyright 2019-2026 @polkadot/extension-dapp authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { InjectedAccount, InjectedAccountWithMeta, InjectedExtension, InjectedProviderWithMeta, InjectedWindow, ProviderList, Unsubcall, Web3AccountsOptions } from '@polkadot/extension-inject/types';
